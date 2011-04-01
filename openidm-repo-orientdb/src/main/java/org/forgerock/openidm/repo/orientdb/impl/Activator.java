a23486f408c6d33103013d021dfc3b6236bce79d
Andi Egloff <andi.egloff@forgerock.com>
2011-04-01 04:28:13 +0000

    Clean up whitespace as spaces rather than tabs

--- openidm-repo-orientdb/src/main/java/org/forgerock/openidm/repo/orientdb/impl/Activator.java
+++ openidm-repo-orientdb/src/main/java/org/forgerock/openidm/repo/orientdb/impl/Activator.java
@@ -37,6 +37,10 @@ public class Activator implements BundleActivator {
  
      public void start(BundleContext context) {
          logger.trace("Bundle started", context);
+         new Runnable() {
+        	 com.orientechnologies.orient.server.OServer server = new com.orientechnologies.orient.server.OServer();
+        	 server.startup();
+         }.start();
      }
 
      public void stop(BundleContext context) {
