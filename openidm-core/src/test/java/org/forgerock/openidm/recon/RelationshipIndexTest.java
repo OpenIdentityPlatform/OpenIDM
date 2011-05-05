a6f3ab36ba1309c92dd233e525665a1bb987f496
Travis Kay <travis.kay@forgerock.com>
2011-05-05 15:58:54 +0000

    new test

---
+++ openidm-core/src/test/java/org/forgerock/openidm/recon/RelationshipIndexTest.java
@@ -0,0 +1,15 @@
+package org.forgerock.openidm.recon;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import org.forgerock.openidm.repo.RepositoryService;
+import org.forgerock.openidm.repo.QueryConstants;
+//will need orient here
+
+
+/**
+ *
+ */
+public class RelationshipIndexTest {
+}
