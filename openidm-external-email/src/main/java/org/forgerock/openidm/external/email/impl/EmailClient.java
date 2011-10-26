d2375bb8d79245cbfadfa2ca21835c6d1b56c1d7
Gael Allioux <gael.allioux@forgerock.com>
2011-10-26 19:41:44 +0000

    initial check-in of external email service

---
+++ openidm-external-email/src/main/java/org/forgerock/openidm/external/email/impl/EmailClient.java
@@ -0,0 +1,174 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright © 2011 ForgeRock AS. All rights reserved.
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://forgerock.org/license/CDDLv1.0.html
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at http://forgerock.org/license/CDDLv1.0.html
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ */
+package org.forgerock.openidm.external.email.impl;
+
+import java.util.Map;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.forgerock.json.fluent.JsonNode;
+
+/**
+ *
+ * @author gael
+ */
+public class EmailClient {
+
+    private String host = "localhost";
+    private String port = "25";
+    private String username = null;
+    private String password = null;
+    private String fromAddr = null;
+    private boolean smtpAuth = false;
+    private Properties props = new Properties();
+    private Session session;
+
+    public EmailClient(Map config) throws RuntimeException {
+        if ((String) config.get("host") != null) {
+            host = (String) config.get("host");
+            props.put("mail.smtp.host", host);
+        }
+        if ((String) config.get("port") != null) {
+            port = (String) config.get("port");
+            props.put("mail.smtp.port", port);
+        }
+        if ((String) config.get("from") != null) {
+            fromAddr = (String) config.get("from");
+        }
+
+        if (((String) config.get("mail.smtp.auth")).equalsIgnoreCase("true")) {
+            if ((String) config.get("username") != null) {
+                username = (String) config.get("username");
+            } else {
+                throw new RuntimeException("No username provided for SMTP auth");
+            }
+            if ((String) config.get("password") != null) {
+                password = (String) config.get("password");
+            } else {
+                throw new RuntimeException("No password provided for SMTP auth");
+            }
+            props.put("mail.smtp.auth", "true");
+            smtpAuth = true;
+        }
+
+        if (((String) config.get("mail.smtp.starttls.enable")).equalsIgnoreCase("true")) {
+            props.put("mail.smtp.starttls.enable", "true");
+        }
+
+        session = Session.getInstance(props);
+    }
+
+    public void send(Map<String, Object> params) throws RuntimeException {
+        // _from : the From: address
+        // _to : The To: recipients - a comma separated email address strings 
+        // _cc: The Cc: recipients - a comma separated email address strings 
+        // _bcc: The Bcc: recipients - a comma separated email address strings 
+        // _subject: The subject
+        // _body : the message body
+        InternetAddress from = null;
+        InternetAddress[] to = null;
+        InternetAddress[] cc = null;
+        InternetAddress[] bcc = null;
+        
+
+        String text = (String) params.get("_body");
+        if (text == null){
+            text = "<empty message>";
+        }
+        String subject = (String) params.get("_subject");
+        if (subject == null){
+            subject = "<no subject>";
+        }
+
+        try {
+            if (params.get("_from") != null) {
+                from = new InternetAddress((String)params.get("_from"));
+            } else {
+                from = new InternetAddress(fromAddr);
+            }
+        } catch (AddressException ae) {
+            throw new RuntimeException("Bad From: header");
+        }
+
+        try {
+            to = InternetAddress.parse((String)params.get("_to"));
+        } catch (AddressException ae) {
+            throw new RuntimeException("Bad To: header");
+        }
+
+        try {
+            if (params.get("_cc") != null) {
+                cc = InternetAddress.parse((String)params.get("_cc"));
+            }
+        } catch (AddressException ae) {
+            throw new RuntimeException("Bad Cc: header");
+        }
+
+        try {
+            if (params.get("_bcc") != null) {
+                bcc = InternetAddress.parse((String)params.get("_bcc"));
+            }
+        } catch (AddressException ae) {
+            throw new RuntimeException("Bad Bcc: header");
+        }
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(from);
+            message.setRecipients(Message.RecipientType.TO, to);
+            if (cc != null) {
+                message.setRecipients(Message.RecipientType.CC, cc);
+            }
+            if (bcc != null) {
+                message.setRecipients(Message.RecipientType.BCC, bcc);
+            }
+            message.setSubject(subject);
+            message.setText(text);
+
+            Transport transport = session.getTransport("smtp");
+
+            if (smtpAuth) {
+                transport.connect(username, password);
+            } else {
+                transport.connect();
+            }
+            message.saveChanges();
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void format() {
+    }
+}
---
+++ openidm-external-email/src/main/java/org/forgerock/openidm/external/email/impl/EmailServiceImpl.java
@@ -0,0 +1,192 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright © 2011 ForgeRock AS. All rights reserved.
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://forgerock.org/license/CDDLv1.0.html
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at http://forgerock.org/license/CDDLv1.0.html
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ */
+package org.forgerock.openidm.external.email.impl;
+
+import org.forgerock.openidm.external.email.EmailService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.osgi.service.component.ComponentContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.ConfigurationPolicy;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Deactivate;
+
+import org.forgerock.json.fluent.JsonNode;
+import org.forgerock.openidm.config.EnhancedConfig;
+import org.forgerock.openidm.config.JSONEnhancedConfig;
+import org.forgerock.openidm.objset.ForbiddenException;
+import org.forgerock.openidm.objset.ObjectSetException;
+import org.forgerock.openidm.objset.Patch;
+
+/**
+ * Email service implementation
+ * @author gael
+ */
+@Component(name = "org.forgerock.openidm.external.email", immediate = true, policy = ConfigurationPolicy.REQUIRE)
+@Service
+@Properties({
+    @Property(name = "service.description", value = "Outbound Email Service"),
+    @Property(name = "service.vendor", value = "ForgeRock AS"),
+    @Property(name = "openidm.router.prefix", value = EmailService.ROUTER_PREFIX)
+})
+public class EmailServiceImpl implements EmailService {
+    final static Logger logger = LoggerFactory.getLogger(EmailServiceImpl.class);
+    public static final String PID = "org.forgerock.openidm.external.email";
+    
+    EnhancedConfig enhancedConfig = new JSONEnhancedConfig();
+    EmailClient emailClient;
+
+    /**
+     * Gets an object from the audit logs by identifier. The returned object is not validated
+     * against the current schema and may need processing to conform to an updated schema.
+     * <p>
+     * The object will contain metadata properties, including object identifier {@code _id},
+     * and object version {@code _rev} to enable optimistic concurrency
+     *
+     * @param fullId the identifier of the object to retrieve from the object set.
+     * @throws NotFoundException if the specified object could not be found.
+     * @throws ForbiddenException if access to the object is forbidden.
+     * @throws BadRequestException if the passed identifier is invalid
+     * @return the requested object.
+     */
+    @Override
+    public Map<String, Object> read(String fullId) throws ObjectSetException {
+        // TODO
+        return new HashMap<String, Object>();
+    }
+
+    /**
+     * Creates a new object in the object set.
+     * <p>
+     * This method sets the {@code _id} property to the assigned identifier for the object,
+     * and the {@code _rev} property to the revised object version (For optimistic concurrency)
+     *
+     * @param fullId the client-generated identifier to use, or {@code null} if server-generated identifier is requested.
+     * @param obj the contents of the object to create in the object set.
+     * @throws NotFoundException if the specified id could not be resolved.
+     * @throws ForbiddenException if access to the object or object set is forbidden.
+     * @throws PreconditionFailedException if an object with the same ID already exists.
+     */
+    @Override
+    public void create(String fullId, Map<String, Object> obj) throws ObjectSetException {
+        throw new ForbiddenException("Not allowed on external email service");
+    }
+
+    /**
+     * Notification service does not support update.
+     */
+    @Override
+    public void update(String fullId, String rev, Map<String, Object> obj) throws ObjectSetException {
+        throw new ForbiddenException("Not allowed on external email service");
+    }
+
+    /**
+     * Notification service currently does not support delete.
+     *
+     * Deletes the specified object from the object set.
+     *
+     * @param fullId the identifier of the object to be deleted.
+     * @param rev the version of the object to delete or {@code null} if not provided.
+     * @throws NotFoundException if the specified object could not be found.
+     * @throws ForbiddenException if access to the object is forbidden.
+     * @throws ConflictException if version is required but is {@code null}.
+     * @throws PreconditionFailedException if version did not match the existing object in the set.
+     */
+    @Override
+    public void delete(String fullId, String rev) throws ObjectSetException {
+        throw new ForbiddenException("Not allowed on external email service");
+    }
+
+    /**
+     * Notification service does not support patch.
+     */
+    @Override
+    public void patch(String id, String rev, Patch patch) throws ObjectSetException {
+        throw new ForbiddenException("Not allowed on external email service");
+    }
+
+    /**
+     * Performs the query on the specified object and returns the associated results.
+     * <p>
+     * Queries are parametric; a set of named parameters is provided as the query criteria.
+     * The query result is a JSON object structure composed of basic Java types.
+     *
+     * The returned map is structured as follow:
+     * - The top level map contains meta-data about the query, plus an entry with the actual result records.
+     * - The <code>QueryConstants</code> defines the map keys, including the result records (QUERY_RESULT)
+     *
+     * @param fullId identifies the object to query.
+     * @param params the parameters of the query to perform.
+     * @return the query results, which includes meta-data and the result records in JSON object structure format.
+     * @throws NotFoundException if the specified object could not be found.
+     * @throws BadRequestException if the specified params contain invalid arguments, e.g. a query id that is not
+     * configured, a query expression that is invalid, or missing query substitution tokens.
+     * @throws ForbiddenException if access to the object or specified query is forbidden.
+     */
+    @Override
+    public Map<String, Object> query(String fullId, Map<String, Object> params) throws ObjectSetException {
+        // TODO
+        return new HashMap<String, Object>();
+    }
+
+    /**
+     * Notification service does not support actions on audit entries.
+     */
+    @Override
+    public Map<String, Object> action(String fullId, Map<String, Object> params) throws ObjectSetException {
+        Map<String, Object> result = new HashMap();
+        logger.debug("External Email service action called for {} with {}", fullId, params);
+        emailClient.send(params);
+        result.put("status", "OK");
+        return result;
+    }
+
+    @Activate
+    void activate(ComponentContext compContext) {
+        logger.debug("Activating Service with configuration {}", compContext.getProperties());
+        JsonNode config = null;
+        try {
+            config = enhancedConfig.getConfigurationAsJson(compContext);
+            emailClient = new EmailClient(config.asMap());
+            logger.debug("external email client enabled");
+        } catch (RuntimeException ex) {
+            logger.warn("Configuration invalid, can not start external email client service.", ex);
+            throw ex;
+        }
+        logger.info(" external email service started.");
+    }
+
+    @Deactivate
+    void deactivate(ComponentContext compContext) {
+        logger.debug("Deactivating Service {}", compContext.getProperties());
+        logger.info("Notification service stopped.");
+    }
+}
