465078bef056f2c3ab6d3c15336e94cf104874a1
Andi Egloff <andi.egloff@forgerock.com>
2011-12-02 22:22:00 +0000

    Correct license headers

--- openidm-security-jetty/src/main/java/org/forgerock/openidm/http/internal/JettySecurityConfigurator.java
+++ openidm-security-jetty/src/main/java/org/forgerock/openidm/http/internal/JettySecurityConfigurator.java
@@ -1,18 +1,25 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright © 2011 ForgeRock AS. All rights reserved.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://forgerock.org/license/CDDLv1.0.html
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at http://forgerock.org/license/CDDLv1.0.html
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
  */
 package org.forgerock.openidm.http.internal;
 
@@ -48,6 +55,12 @@ public final class JettySecurityConfigurator implements SecurityConfigurator {
         // If embedded Jetty is used, enable security
         if (enabled && httpService instanceof org.ops4j.pax.web.service.WebContainer) {
             org.ops4j.pax.web.service.WebContainer webContainer = (org.ops4j.pax.web.service.WebContainer) httpService;
+/*
+            String authMethod = "FORM";
+            String realmName = "openidm";
+            String formLoginPage = "/login/login";
+            String formErrorPage = "/login/error";
+*/
             String authMethod = "BASIC";
             String realmName = "openidm";
             String formLoginPage = null;
