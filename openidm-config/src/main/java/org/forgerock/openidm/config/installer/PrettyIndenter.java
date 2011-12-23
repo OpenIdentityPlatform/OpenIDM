9449baf6f3fd7ce4b1d29e3ec54937c453285287
Andi Egloff <andi.egloff@forgerock.com>
2011-12-23 16:52:37 +0000

    Add license headers

--- openidm-config/src/main/java/org/forgerock/openidm/config/installer/PrettyIndenter.java
+++ openidm-config/src/main/java/org/forgerock/openidm/config/installer/PrettyIndenter.java
@@ -1,5 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 /**
  * Plug-in based on the AP 2.0 licensed Jackson Indenter
+ * with changes to have configurable indentation.
+ * 
+ * The original Jackson source files do not have license headers
+ * and the above was added to clearly identify the 
+ * licensing of the original file. 
  */
 package org.forgerock.openidm.config.installer;
 
