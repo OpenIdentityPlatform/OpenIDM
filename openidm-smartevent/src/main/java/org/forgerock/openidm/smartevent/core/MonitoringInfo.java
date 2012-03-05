adf530d70e0ee974cd0bace4db9967617819cec1
Andi Egloff <andi.egloff@forgerock.com>
2012-03-05 19:58:46 +0000

    OPENIDM-571 Initial check-in of general purpose eventing framework, which includes the capability to monitor and gather performance statistics in production environments

---
+++ openidm-smartevent/src/main/java/org/forgerock/openidm/smartevent/core/MonitoringInfo.java
@@ -0,0 +1,45 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions Copyrighted [year] [name of copyright owner]".
+ *
+ * Copyright © 2012 ForgeRock AS. All rights reserved.
+ */
+
+package org.forgerock.openidm.smartevent.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Holds monitoring and statistics info 
+ * 
+ * @author aegloff
+ */
+public class MonitoringInfo {
+
+    public long totalInvokes;
+    public long totalTime;
+
+    /**
+     * Reset the statistics
+     */
+    public void reset() {
+        totalInvokes = 0;
+        totalTime = 0;
+    }
+    
+    public String toString() {
+        return "Invocations: " + totalInvokes 
+                + " total time: " + StatisticsHandler.formatNsAsMs(totalTime) 
+                + " mean: " + StatisticsHandler.formatNsAsMs(totalInvokes > 0 ? totalTime/totalInvokes : -1);
+    }
+}
