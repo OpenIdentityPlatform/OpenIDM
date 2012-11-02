115f0479f5b5d39f598881485392406fc8bb7070
Jake Feasel <jake.feasel@forgerock.com>
2012-11-02 21:14:53 +0000

    Fixing copyright notice

--- openidm-zip/src/main/resources/script/policy.js
+++ openidm-zip/src/main/resources/script/policy.js
@@ -1,7 +1,7 @@
 /*! @license 
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright ¬¨¬®¬¨¬Æ¬¨¬®¬¨¬© 2011-2012 ForgeRock AS. All rights reserved.
+ * Copyright (c) 2012 ForgeRock AS. All Rights Reserved
  *
  * The contents of this file are subject to the terms
  * of the Common Development and Distribution License
