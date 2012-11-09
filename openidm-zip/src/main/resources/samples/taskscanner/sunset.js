d3bfe865adc42c7085d286aa8b6bab26769e2a2b
Travis Hall <travis.hall@forgerock.com>
2012-11-09 20:23:18 +0000

    [CR-858] Fixes for OPENIDM-837, 842, 843   * Ignoring revision in managed object set patch should handle MVCC     failure   * Patch function is not working in JavaScript openidm object   * Encrypted fields cause failures upon update when policies are     defined for them

--- openidm-zip/src/main/resources/samples/taskscanner/sunset.js
+++
@@ -1,8 +0,0 @@
-logger.debug("Performing Sunset Task on {} ({})", input.email, objectID);
-
-// Perform sunsetting task - mark inactive, trigger workflow, etc.
-input['active'] = false;
-// Perform update on the object to complete the sunsetting task
-openidm.update(objectID, input['_rev'], input);
-
-true; // return true to indicate successful completion
