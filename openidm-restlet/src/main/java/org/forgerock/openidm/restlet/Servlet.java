31c1ca2152b87aead98b3034811f64133ceb183c
Andi Egloff <andi.egloff@forgerock.com>
2012-11-27 22:11:20 +0000

    OPENIDM-961 [CR-958] Allow for easy integration of access manager servlet filters, provide an OpenAM example

--- openidm-restlet/src/main/java/org/forgerock/openidm/restlet/Servlet.java
+++ openidm-restlet/src/main/java/org/forgerock/openidm/restlet/Servlet.java
@@ -17,12 +17,17 @@
 package org.forgerock.openidm.restlet;
 
 // Java SE
+import java.util.ArrayList;
 import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 // Java Servlet
@@ -64,10 +69,13 @@ import org.restlet.Request;
 import org.restlet.Restlet;
 
 // JSON Fluent
+import org.forgerock.json.fluent.JsonPointer;
 import org.forgerock.json.fluent.JsonValue;
+import org.forgerock.json.fluent.JsonValueException;
 
 // JSON Resource
 import org.forgerock.json.resource.JsonResource;
+import org.forgerock.json.resource.JsonResourceException;
 
 // JSON Resource Restlet
 import org.forgerock.json.resource.restlet.JsonResourceRestlet;
@@ -75,8 +83,13 @@ import org.forgerock.json.resource.restlet.JsonResourceRestlet;
 // Restlet Utilities
 import org.forgerock.restlet.RestletRouterServlet;
 
-// Deprecated
+import org.forgerock.openidm.filterregistration.ServletFilterRegistrator;
+import org.forgerock.openidm.scope.ScopeFactory;
 import org.forgerock.openidm.objset.ObjectSetContext;
+import org.forgerock.openidm.script.Script;
+import org.forgerock.openidm.script.ScriptException;
+import org.forgerock.openidm.script.ScriptThrownException;
+import org.forgerock.openidm.script.Scripts;
 
 /**
  * Servlet to handle the REST interface.
@@ -100,8 +113,10 @@ import org.forgerock.openidm.objset.ObjectSetContext;
 )
 public class Servlet extends RestletRouterServlet {
 
+    private static final long serialVersionUID = 1L;
+    
     /** TODO: Description. */
-    final static Logger LOGGER = LoggerFactory.getLogger(Servlet.class);
+    final static Logger logger = LoggerFactory.getLogger(Servlet.class);
     
     /** TODO: Description. */
     private static final String PATH_PROPERTY = "openidm.restlet.path";
@@ -112,6 +127,9 @@ public class Servlet extends RestletRouterServlet {
     /** TODO: Description. */
     private ServiceRegistration serviceRegistration;
     
+    // Optional scripts to augment/populate the security context
+    List<Script> augmentSecurityContext = new CopyOnWriteArrayList<Script>();
+
     /**
      * Provides automatic binding of {@link Restlet} objects that include the
      * {@code openidm.restlet.path} property.
@@ -156,17 +174,52 @@ public class Servlet extends RestletRouterServlet {
         }
     }
 
+    // Register script extensions configured
+    @Reference(
+        name = "reference_Servlet_ServletFilterRegistrator",
+        referenceInterface = ServletFilterRegistrator.class,
+        bind = "bindRegistrator",
+        unbind = "unbindRegistrator",
+        cardinality = ReferenceCardinality.OPTIONAL_MULTIPLE,
+        policy = ReferencePolicy.DYNAMIC,
+        strategy = ReferenceStrategy.EVENT
+    )
+    protected Map<ServletFilterRegistrator, Script> filterRegistratorMap = 
+            new HashMap<ServletFilterRegistrator, Script>();
+    protected synchronized void bindRegistrator(ServletFilterRegistrator registrator, 
+            Map<String, Object> properties) {
+        JsonValue regConfig = registrator.getConfiguration();
+        JsonValue scriptCfg = regConfig.get("scriptExtensions").get("augmentSecurityContext");
+        if (!scriptCfg.isNull()) {
+            Script augmentScript = Scripts.newInstance("Servlet", scriptCfg);
+            filterRegistratorMap.put(registrator, augmentScript);
+            augmentSecurityContext.add(augmentScript);
+            logger.debug("Registered script {}", augmentScript);
+        }
+    }
+    
+    protected synchronized void unbindRegistrator(ServletFilterRegistrator registrator, 
+            Map<String, Object> properties) {
+        Script augmentScript = filterRegistratorMap.remove(registrator);
+        if (augmentScript != null) {
+            augmentSecurityContext.remove(augmentScript);
+            logger.debug("Deregistered script {}", augmentScript);
+        }
+    }
+
+    @Reference
+    public ScopeFactory scopeFactory;
+
+    private Map<String, Object> newScope() {
+        return scopeFactory.newInstance(ObjectSetContext.get());
+    }
+
     @Activate
     protected synchronized void activate(ComponentContext context) throws ServletException, NamespaceException {
         this.context = context;
-        String alias = "/openidm";
-        /*Dictionary<String, Object> props = new Hashtable<String, Object>();
-        props.put(ExtenderConstants.PROPERTY_ALIAS, alias);
-        //props.put(ExtenderConstants.PROPERTY_URL_PATTERNS,"openidm");
-        props.put(ExtenderConstants.PROPERTY_HTTP_CONTEXT_ID, "openidm");
-        props.put("servlet-name", "OpenIDM REST");
-        serviceRegistration = context.getBundleContext().registerService(javax.Servlet.class.getName(), this, props);*/
+        JsonValue config = new JsonValue(new HashMap());
 
+        String alias = "/openidm";
         DefaultServletMapping servletMapping = new DefaultServletMapping();
         servletMapping.setHttpContextId("openidm");
         servletMapping.setAlias(alias);
@@ -174,7 +227,7 @@ public class Servlet extends RestletRouterServlet {
         servletMapping.setServletName("OpenIDM REST");
         //All WebApplication elements must be registered with the same BundleContext
         serviceRegistration = FrameworkUtil.getBundle(ContextRegistrator.class).getBundleContext().registerService(ServletMapping.class.getName(), servletMapping, null);
-        LOGGER.debug("Registered servlet at {}", alias);
+        logger.debug("Registered servlet at {}", alias);
     }
 
     @Deactivate
@@ -197,31 +250,152 @@ public class Servlet extends RestletRouterServlet {
     }
 
     private class CustomRestlet extends JsonResourceRestlet {
+        private static final String USERID_ID = "id";
+        public static final String USERID_COMPONENT = "component";
+        public static final String OPENIDM_ROLES = "openidm-roles";
+        public static final String USERID = "userid";
+        public static final String USERNAME = "username";
+
         public CustomRestlet(JsonResource resource) {
             super(resource);
         }
         @Override public JsonValue newContext(Request request) {
             JsonValue result = super.newContext(request);
             JsonValue security = result.get("security");
-            Map<String, Object> qualifiedId = new LinkedHashMap<String, Object>();
-            qualifiedId.put("component", request.getAttributes().get("openidm.resource"));
-            qualifiedId.put("id", request.getAttributes().get("openidm.userid"));
-            security.put("userid", qualifiedId);
-            security.put("openidm-roles", request.getAttributes().get("openidm.roles"));
-            
-            // TODO: Remove this temporary compatibility code
-            // TODO: Temporarily left in for compatibility until UI migrates to new capabilities
-            String resource = (String) request.getAttributes().get("openidm.resource");
-            resource = resource.replace('/', '_'); // Current use is with OrientDB class names
-            security.put("openidm-resource", resource);
-            String oldnamedEntry = security.get("user").asString(); // temporary compatibilty with previous commons
-            if (oldnamedEntry != null) {
-                security.put("username", oldnamedEntry);
-                security.remove("user");
+
+            JsonValue attrs = new JsonValue(request.getAttributes());
+            try {
+                JsonValue paramUserName = singleEntry(attrs, "openidm.username");
+                JsonValue paramUserResource = singleEntry(attrs, "openidm.resource");
+                JsonValue paramUserLocalId = singleEntry(attrs, "openidm.userid");
+                JsonValue paramRoles = listEntry(attrs, "openidm.roles");
+                JsonValue authInvoked = singleEntry(attrs, "openidm.authinvoked");
+
+                // There must be at least the authInvoked param set to indicate that 
+                // the security filter has protected the call
+                if (!authInvoked.isNull()) {
+                    // If not passed in the principal, user name must be supplied in params
+                    if (security.get(USERNAME).isNull()) {
+                        logger.debug("username not populated from principal, try to get from params {}", security);
+                        security.put(USERNAME, paramUserName.required().asString());
+                    }
+                    Map<String, Object> qualifiedId = new LinkedHashMap<String, Object>();
+                    if (!paramUserResource.isNull()) {
+                        qualifiedId.put(USERID_COMPONENT, paramUserResource.asString());
+                    }
+                    if (!paramUserLocalId.isNull()) {
+                        qualifiedId.put(USERID_ID, paramUserLocalId.asString());
+                    }
+                    if (!paramUserResource.isNull() || !paramUserLocalId.isNull()) {
+                        security.put(USERID, qualifiedId);
+                    }
+                    if (!paramRoles.isNull()) {
+                        security.put(OPENIDM_ROLES, paramRoles.asList());
+                    }
+
+                    // Invoke augment script if defined
+                    if (augmentSecurityContext != null && augmentSecurityContext.size() > 0) {
+                        for (Script augmentScript : augmentSecurityContext) {
+                            augmentContext(augmentScript, request, security);
+                        }
+                    }
+
+                    // Check security context fully populated
+                    security.get(USERNAME).required();
+                    security.get(USERID).required();
+                    security.get(USERID).get(USERID_COMPONENT).required();
+                    security.get(USERID).get(USERID_ID).required();
+                    security.get(OPENIDM_ROLES).required();
+                } else {
+                    logger.warn("Rejecting invocation as required context to allow invocation not populated");
+                    throw new RuntimeException("Rejecting invocation as required context to allow invocation not populated");
+                }
+            } catch (JsonValueException ex) {
+                logger.warn("Security context not populated correctly: {}", ex.getMessage(), ex);
+                throw ex;
+            } catch (JsonResourceException ex) {
+                logger.warn("Failure in augmenting security context: {}", ex.getMessage(), ex);
+                throw new RuntimeException("Failure in augmenting security context: " 
+                            + ex.getMessage(), ex);
             }
-            
-            
+
+            logger.debug("New populated context: {}", result);
             return result;
+        }
+        
+        private void augmentContext(Script augmentScript, Request request, JsonValue securityContext) throws JsonResourceException {
+            // Pass request and current security context to augmeet
+            Map<String, Object> scope = newScope();
+
+            try {            
+                scope.put("request", request);
+                scope.put("security", securityContext.getObject());
+                Object ret = augmentScript.exec(scope);
+            } catch (ScriptThrownException ste) {
+                throw ste.toJsonResourceException(null);
+            } catch (ScriptException se) {
+                throw se.toJsonResourceException("Failure in executing security context augment script: "
+                        + se.getMessage());
+            } 
+        }
+
+        /**
+         * Gets a single entry from attributes at the given key
+         * For String, Number, Boolean value these are returned as is
+         * For List or Set, the first entry is returned
+         * For other types, null is returned
+         * @param attributes the list of attributes
+         * @param key the attribute key
+         * @return the single entry, wrapped in JsonValue convenience wrapper
+         */
+        private JsonValue singleEntry(JsonValue attributes, String key) {
+            JsonValue value = attributes.get(key);
+            if (value.isString() || value.isNumber() || value.isBoolean()) {
+                return value;
+            } else if (value.isList()){
+                if (value.size() > 1) {
+                    logger.warn("Expecting only one paramter in {} List parameter, passed {}",
+                            key, value.size());
+                }
+                return value.get(0); // Returns null if list was empty
+            } else {
+                // JsonValue doesn't contain convenience functions for Set yet
+                Object rawValue = value.getObject();
+                if (rawValue instanceof Set) {
+                    Iterator iter = ((Set) rawValue).iterator();
+                    if (iter.hasNext()) {
+                        Object firstEntry = iter.next();
+                        if (iter.hasNext()) {
+                            logger.warn("Expecting only one paramter in {} Set parameter, passed {}",
+                                    key, value.size());
+                        }
+                        return new JsonValue(firstEntry);
+                    }
+                }
+            }
+            return new JsonValue(null, new JsonPointer(key));
+        }
+
+        /**
+         * Gets a List from attributes at the given key
+         * For a value of type List, it is returned as is
+         * For a value of type Set, it is converted and returned as List
+         * @param attributes the list of attributes
+         * @param key the attribute key
+         * @return the List entry, wrapped in JsonValue convenience wrapper
+         */
+        private JsonValue listEntry(JsonValue attributes, String key) {
+            JsonValue value = attributes.get(key);
+            if (value.isList()){
+                return value;
+            } else {
+                // JsonValue doesn't contain convenience functions for Set yet
+                Object rawValue = value.getObject();
+                if (rawValue instanceof Set) {
+                    return new JsonValue(new ArrayList<Object>((Set)rawValue));
+                }
+            }
+            return new JsonValue(null, new JsonPointer(key));
         }
     }
 }
---
+++ openidm-servletfilter-registrator/src/main/java/org/forgerock/openidm/filterregistration/impl/ServletFilterRegistratorSvc.java
@@ -0,0 +1,257 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions Copyrighted [year] [name of copyright owner]".
+ *
+ * Copyright © 2012 ForgeRock AS. All rights reserved.
+ */
+
+package org.forgerock.openidm.filterregistration.impl;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.Filter;
+import javax.servlet.ServletRequest;
+
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.ConfigurationPolicy;
+import org.apache.felix.scr.annotations.Deactivate;
+import org.apache.felix.scr.annotations.Service;
+import org.forgerock.json.fluent.JsonValue;
+import org.forgerock.openidm.config.JSONEnhancedConfig;
+import org.forgerock.openidm.filterregistration.ServletFilterRegistrator;
+import org.forgerock.openidm.http.ContextRegistrator;
+import org.ops4j.pax.web.extender.whiteboard.FilterMapping;
+import org.ops4j.pax.web.extender.whiteboard.runtime.DefaultFilterMapping;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.ComponentContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Takes configuration to register and de-register configured servlet filters, 
+ * with support to load the filter or supporting classes off a defined class path
+ * @author aegloff
+ */
+
+@Component(
+    name = "org.forgerock.openidm.servletfilter", 
+    immediate = true,
+    policy = ConfigurationPolicy.REQUIRE,
+    configurationFactory=true
+)
+@Service
+public class ServletFilterRegistratorSvc implements ServletFilterRegistrator {
+    private final static Logger logger = LoggerFactory.getLogger(ServletFilterRegistratorSvc.class);
+
+    // Context of this scr component 
+    private ComponentContext context;
+    
+    // Comfig received for this component
+    private JsonValue config;
+
+    // Handle to registered servlet filter
+    private ServiceRegistration serviceRegistration;
+    
+    // Original setting of system properties
+    Map<String, String> origSystemProperties = new HashMap<String, String>();
+    
+    Map<String, Object> preInvokeReqAttributes;
+
+    @Activate
+    protected synchronized void activate(ComponentContext context) throws Exception {
+        this.context = context;
+        logger.info("Activating servlet registratrator with configuration {}", context.getProperties());
+        config = new JsonValue(new JSONEnhancedConfig().getConfiguration(context));
+
+        logger.debug("Parsed servlet filter config: {}", config);
+
+        // Get required info from config
+        String filterClass = config.get("filterClass").required().asString();
+        logger.info("Using filter class: {}", filterClass);
+        JsonValue urlStrings = config.get("classPathURLs");
+        List<URL> urls = new ArrayList<URL>();
+        for (JsonValue urlStr : urlStrings.expect(List.class)) {
+            try {
+                URL url = new URL(urlStr.asString());
+                urls.add(url);
+                logger.info("Added URL to filter classpath: {}", url);
+            } catch (MalformedURLException ex) {
+                logger.warn("Configured classPathURL is not a valid URL: {}", urlStr, ex);
+                throw ex;
+            }
+        }
+        origSystemProperties = new HashMap<String, String>();
+        JsonValue rawSystemProperties = config.get("systemProperties");
+        for (String key : rawSystemProperties.keys()) {
+            String prev = System.setProperty(key, rawSystemProperties.get(key).asString());
+            // null value is used to keep track of properties that weren't set before
+            origSystemProperties.put(key, prev);
+        }
+        preInvokeReqAttributes = 
+                (Map<String, Object>) config.get("requestAttributes").asMap();
+        
+        String httpContextId = config.get("httpContextId").defaultTo("openidm").asString();
+
+        // Servlet names this filter should apply to, e.g. one could also add "OpenIDM Web"
+        List<String> servletNames = null;
+        JsonValue rawServletNames = config.get("servletNames");
+        if (rawServletNames.isNull()) {
+            // default
+            servletNames = new ArrayList<String>();
+            servletNames.add("OpenIDM REST");
+        } else {
+            servletNames = rawServletNames.asList(String.class);
+        }
+
+        // URL patterns to apply the filter to, e.g. one could also add "/openidmui/*");
+        List<String> urlPatterns = null;
+        JsonValue rawUrlPatterns = config.get("urlPatterns");
+        if (rawUrlPatterns.isNull()) {
+            // default
+            urlPatterns = new ArrayList<String>();
+            urlPatterns.add("/openidm/*");
+        } else {
+            urlPatterns = rawUrlPatterns.asList(String.class);
+        }
+
+        // Filter init params, a string to string map
+        JsonValue rawInitParams = config.get("initParams");
+        Map<String, String> initParams = null;
+        if (!rawInitParams.isNull()) {
+            initParams = new HashMap<String, String>();
+        }
+        for (String initParamKey : rawInitParams.keys()) {
+            initParams.put(initParamKey, rawInitParams.get(initParamKey).asString());
+        }
+
+        // Create a classloader and dynamically create the requested filter
+        Filter filter = null; 
+        ClassLoader filterCL = null;
+        ClassLoader origCL = Thread.currentThread().getContextClassLoader();
+        try {
+            filterCL = new URLClassLoader(urls.toArray(new URL[0]), this.getClass().getClassLoader());
+            Thread.currentThread().setContextClassLoader(filterCL);
+            filter = (Filter)(Class.forName(filterClass, true, filterCL).newInstance());
+        } catch (Exception ex) {
+            logger.warn("Configured class {} failed to load from configured class path URLs {}", new Object[] {filterClass, urls, ex});
+            throw ex;
+        } finally {
+            Thread.currentThread().setContextClassLoader(origCL);
+        }
+
+        Filter proxiedFilter = (Filter)
+                Proxy.newProxyInstance(
+                        filter.getClass().getClassLoader(),
+                        new Class[] { Filter.class },
+                        new FilterProxy(filter, filterCL, preInvokeReqAttributes));
+        
+        DefaultFilterMapping filterMapping = new DefaultFilterMapping();
+        filterMapping.setFilter(proxiedFilter);
+        filterMapping.setHttpContextId(httpContextId);
+        filterMapping.setServletNames(servletNames.toArray(new String[0]));
+        filterMapping.setUrlPatterns(urlPatterns.toArray(new String[0]));
+        filterMapping.setInitParams(initParams);
+        serviceRegistration = FrameworkUtil.getBundle(ContextRegistrator.class).getBundleContext().registerService(FilterMapping.class.getName(), filterMapping, null);
+        logger.info("Successfully registered servlet filter {}", context.getProperties());
+    }
+
+    @Deactivate
+    protected synchronized void deactivate(ComponentContext context) {
+        if (serviceRegistration != null) {
+            try {
+                serviceRegistration.unregister();
+                logger.info("Unregistered servlet filter {}.", context.getProperties());
+            } catch (Exception ex) {
+                logger.warn("Failure reported during unregistering of servlet filter {}: {}", 
+                        new Object[]{context.getProperties(), ex.getMessage(), ex});
+            }
+        }
+
+        // Restore the system properties before this config was applied
+        for (String key : origSystemProperties.keySet()) {
+            String val = origSystemProperties.get(key);
+            if (val == null) {
+                System.clearProperty(key);
+            } else {
+                System.setProperty(key, val);
+            }
+        }
+        logger.debug("Deactivated {}", context);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public JsonValue getConfiguration() {
+        return config;
+    }
+    
+    /**
+     * Wraps Filter to set the thread context classloader
+     * @author aegloff
+     */
+    private static class FilterProxy implements InvocationHandler {
+        Filter filter;
+        ClassLoader threadCtxClassloader;
+        Map<String, Object> preInvokeReqAttributes;
+        /**
+         * Sets the thread context for a filter invocation to the desired value
+         * @param filter filter to wrap
+         * @param threadCtxClassloader classloader to set as thread context classloader
+         * @param preInvokeReqAttributes request attributes to set before the filter doFilter is invoked 
+         */
+        public FilterProxy(Filter filter, ClassLoader threadCtxClassloader, 
+                Map<String, Object> preInvokeReqAttributes) { 
+            this.filter = filter;
+            this.threadCtxClassloader = threadCtxClassloader;
+            this.preInvokeReqAttributes = preInvokeReqAttributes;
+        }
+
+        public Object invoke(Object proxy, Method m, Object[] args) throws Throwable {
+            ClassLoader origCL = Thread.currentThread().getContextClassLoader();
+            try {
+                Thread.currentThread().setContextClassLoader(threadCtxClassloader);
+                if (preInvokeReqAttributes != null) {
+                    if (m.getName().equals("doFilter") && args != null && args.length == 3 && args[0] instanceof ServletRequest) {
+                        ServletRequest request = (ServletRequest) args[0];
+                        for (Map.Entry<String, Object> entry : preInvokeReqAttributes.entrySet()) {
+                            logger.trace("Adding attribute to doFilter request {}", entry);
+                            request.setAttribute(entry.getKey(), entry.getValue());
+                        }
+                    }
+                }
+                Object val = m.invoke(filter, args);
+                return val;
+            } catch (InvocationTargetException e) {
+                logger.debug("Filter invocation InvocationTargetException", e.getTargetException());
+                throw e.getTargetException();
+            } catch (Exception e) {
+                logger.debug("Filter invocation Exception", e);
+                throw e;
+            } finally {
+                Thread.currentThread().setContextClassLoader(origCL);
+            }
+        }
+    }
+}
+
