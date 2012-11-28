768813c0a7e0abb147ea4c2a38243613205cf8ff
Jake Feasel <jake.feasel@forgerock.com>
2012-11-28 01:58:25 +0000

    Changes to policy code to use new "context" idea

--- openidm-zip/src/main/resources/bin/defaults/script/policy.js
+++ openidm-zip/src/main/resources/bin/defaults/script/policy.js
@@ -52,41 +52,41 @@ var policyConfig = {
             },
             {
                 "policyId" : "valid-date",
-                "policyExec" : "validDateWhenPresent",
+                "policyExec" : "validDateWhenRequired",
                 "clientValidation": true,
                 "policyRequirements": ["VALID_DATE"]
             },
             {
                 "policyId" : "valid-email-address-format",
-                "policyExec" : "validEmailAddressFormatWhenPresent",
+                "policyExec" : "validEmailAddressFormatWhenRequired",
                 "clientValidation": true,
                 "policyRequirements": ["VALID_EMAIL_ADDRESS_FORMAT"]
             },
             {
                 "policyId" : "valid-name-format",
-                "policyExec" : "validNameFormatWhenPresent",
+                "policyExec" : "validNameFormatWhenRequired",
                 "clientValidation": true,
                 "policyRequirements": ["VALID_NAME_FORMAT"]
             },
             {
                 "policyId" : "valid-phone-format",
-                "policyExec" : "validPhoneFormatWhenPresent",
+                "policyExec" : "validPhoneFormatWhenRequired",
                 "clientValidation": true,
                 "policyRequirements": ["VALID_PHONE_FORMAT"]
             },
             {   "policyId" : "at-least-X-capitals",
                 "clientValidation": true,
-                "policyExec" : "atLeastXCapitalLettersWhenPresent", 
+                "policyExec" : "atLeastXCapitalLettersWhenRequired", 
                 "policyRequirements" : ["AT_LEAST_X_CAPITAL_LETTERS"]
             },
             {   "policyId" : "at-least-X-numbers",
                 "clientValidation": true,
-                "policyExec" : "atLeastXNumbersWhenPresent", 
+                "policyExec" : "atLeastXNumbersWhenRequired", 
                 "policyRequirements" : ["AT_LEAST_X_NUMBERS"]
             },
             {   "policyId" : "minimum-length",
                 "clientValidation": true,
-                "policyExec" : "minLengthWhenPresent", 
+                "policyExec" : "minLengthWhenRequired", 
                 "policyRequirements" : ["MIN_LENGTH"]
             },
             {   "policyId" : "cannot-contain-others",
@@ -106,6 +106,38 @@ var policyConfig = {
             }
         ] 
     };
+
+// this function must be defined and identical on both the client and the server side if it to be used in a 
+// function that is identified as "clientValidation": true
+var checkIfRequiredApplies = function(allPolicyRequirements,failedPolicyRequirements,value) {
+    var i,j,
+        hasRequired=false,
+        requireFailed=false;
+    
+    if (typeof(allPolicyRequirements) !== "undefined") {
+        for (i in allPolicyRequirements) {
+            if (allPolicyRequirements[i]  === "REQUIRED") {
+                hasRequired = true;
+                break;
+            }
+        }
+    }
+    if (hasRequired && typeof(failedPolicyRequirements) !== "undefined") {
+        for (j in failedPolicyRequirements) {
+            if (failedPolicyRequirements[j].policyRequirement  === "REQUIRED") {
+                requireFailed = true;
+                break;
+            }
+        }
+    }
+    // this means that the 'required' flag has not been set, even though this field is empty; 
+    // this indicates the required attribute is not being enforced 
+    if (hasRequired && !requireFailed && !(typeof(value) === "string" && value.length)) { 
+        return false; // do not attempt to impose this policy rule on this optional field
+    } else {
+        return hasRequired;
+    }
+};
 
 function required(fullObject, value, params, propName) {
     if (value === undefined) {
@@ -135,7 +167,7 @@ function maxAttemptsTriggersLockCooldown(fullObject, value, params, property) {
 
 function noInternalUserConflict(fullObject, value, params, property) {
     if (value && value.length) {
-    	var queryParams = {
+        var queryParams = {
             "_queryId": "credential-internaluser-query",
             "username": value
             },
@@ -177,60 +209,79 @@ function unique(fullObject, value, params, property) {
     return [];
 }
 
-function validDateWhenPresent(fullObject, value, params, property) {
-    if (typeof(value) === "string" && value.length && isNaN(new Date(value).getTime())) {
+function validDateWhenRequired(fullObject, value, params, property) {
+    if (checkIfRequiredApplies(this.allPolicyRequirements, this.failedPolicyRequirements, value) && 
+        (typeof(value) !== "string" || !value.length || isNaN(new Date(value).getTime()))) {
         return [ {"policyRequirement": "VALID_DATE"}];
-    }
-    else
+    } else {
         return [];
+    }
 }
 
-function validPhoneFormatWhenPresent(fullObject, value, params, property) {
+function validPhoneFormatWhenRequired(fullObject, value, params, property) {
     var phonePattern = /^\+?([0-9\- \(\)])*$/;
-    if (typeof(value) === "string" && value.length && !phonePattern.test(value))
+
+    if (checkIfRequiredApplies(this.allPolicyRequirements, this.failedPolicyRequirements, value) && 
+        (typeof(value) !== "string" || !value.length || !phonePattern.test(value))) {
         return [ {"policyRequirement": "VALID_PHONE_FORMAT"}];
-    else
+    } else {
         return [];
+    }
 }
 
-function validNameFormatWhenPresent(fullObject, value, params, property) {
+function validNameFormatWhenRequired(fullObject, value, params, property) {
     var namePattern = /^([A-Za'-\u0105\u0107\u0119\u0142\u00F3\u015B\u017C\u017A\u0104\u0106\u0118\u0141\u00D3\u015A\u017B\u0179\u00C0\u00C8\u00CC\u00D2\u00D9\u00E0\u00E8\u00EC\u00F2\u00F9\u00C1\u00C9\u00CD\u00D3\u00DA\u00DD\u00E1\u00E9\u00ED\u00F3\u00FA\u00FD\u00C2\u00CA\u00CE\u00D4\u00DB\u00E2\u00EA\u00EE\u00F4\u00FB\u00C3\u00D1\u00D5\u00E3\u00F1\u00F5\u00C4\u00CB\u00CF\u00D6\u00DC\u0178\u00E4\u00EB\u00EF\u00F6\u00FC\u0178\u00A1\u00BF\u00E7\u00C7\u0152\u0153\u00DF\u00D8\u00F8\u00C5\u00E5\u00C6\u00E6\u00DE\u00FE\u00D0\u00F0\-\s])+$/;
-    if (typeof(value) === "string" && value.length && !namePattern.test(value))
+
+    if (checkIfRequiredApplies(this.allPolicyRequirements, this.failedPolicyRequirements, value) && 
+        (typeof(value) !== "string" || !value.length || !namePattern.test(value))) {
         return [ {"policyRequirement": "VALID_NAME_FORMAT"}];
-    else
+    } else {
         return [];
+    }
 }
 
-function minLengthWhenPresent(fullObject, value, params, property) { 
+function minLengthWhenRequired(fullObject, value, params, property) {
     var minLength = params.minLength;
-    if (typeof(value) === "string" && value.length && value.length < minLength) {
+    
+    if (checkIfRequiredApplies(this.allPolicyRequirements, this.failedPolicyRequirements, value) && 
+        (typeof(value) !== "string" || !value.length || value.length < minLength)) {
         return [ { "policyRequirement" : "MIN_LENGTH", "params" : {"minLength":minLength} } ];
-    }
-    return [];
-} 
-
-function atLeastXCapitalLettersWhenPresent(fullObject, value, params, property) {
-    var reg = /[(A-Z)]/g;
-    if (typeof value === "string" && value.length && (value.match(reg) === null || value.match(reg).length < params.numCaps)) {
-        return [ { "policyRequirement" : "AT_LEAST_X_CAPITAL_LETTERS", "params" : {"numCaps": params.numCaps} } ];
-    }
-    return [];
-}
-
-function atLeastXNumbersWhenPresent(fullObject, value, params, property) {
-    var reg = /\d/g;
-    if (typeof value === "string" && value.length && (value.match(reg) === null || value.match(reg).length < params.numNums)) {
-        return [ { "policyRequirement" : "AT_LEAST_X_NUMBERS", "params" : {"numNums": params.numNums}  } ];
-    }
-    return [];
-}
-
-function validEmailAddressFormatWhenPresent(fullObject, value, params, property) {
-    var emailPattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/; 
-    if (typeof value === "string" && value.length && !emailPattern.test(value))
-        return [ {"policyRequirement": "VALID_EMAIL_ADDRESS_FORMAT"}];
-    else
+    } else {
         return [];
+    }
+}
+
+function atLeastXCapitalLettersWhenRequired(fullObject, value, params, property) {
+    var reg = /[(A-Z)]/g;
+    
+    if (checkIfRequiredApplies(this.allPolicyRequirements, this.failedPolicyRequirements, value) && 
+        (typeof(value) !== "string" || !value.length || value.match(reg) === null || value.match(reg).length < params.numCaps)) {
+        return [ { "policyRequirement" : "AT_LEAST_X_CAPITAL_LETTERS", "params" : {"numCaps": params.numCaps} } ];
+    } else {
+        return [];
+    }
+}
+
+function atLeastXNumbersWhenRequired(fullObject, value, params, property) {
+    var reg = /\d/g;
+    
+    if (checkIfRequiredApplies(this.allPolicyRequirements, this.failedPolicyRequirements, value) && 
+        (typeof(value) !== "string" || !value.length || value.match(reg) === null || value.match(reg).length < params.numNums)) {
+        return [ { "policyRequirement" : "AT_LEAST_X_NUMBERS", "params" : {"numNums": params.numNums}  } ];
+    } else {
+        return [];
+    }
+}
+
+function validEmailAddressFormatWhenRequired(fullObject, value, params, property) {
+    var emailPattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/; 
+    
+    if (checkIfRequiredApplies(this.allPolicyRequirements, this.failedPolicyRequirements, value) && 
+        (typeof(value) !== "string" || !value.length || !emailPattern.test(value))) {
+        return [ {"policyRequirement": "VALID_EMAIL_ADDRESS_FORMAT"}];
+    } else {
+        return [];
+    }
 }
 
 function cannotContainOthers(fullObject, value, params, property) {
@@ -438,9 +489,18 @@ function getResourceWithPolicyRequirements(resource) {
     return resource;
 }
 
+function getAllPolicyRequirements(policies) {
+    var reqs = [],i;
+    for (i in policies) {
+        reqs = reqs.concat(getPolicy(policies[i].policyId).policyRequirements);
+    }
+    return reqs;
+}
+
 function validate(policies, fullObject, propName, propValue, retArray) {
     var retObj = {},
         policyRequirements = new Array(),
+        allPolicyRequirements = getAllPolicyRequirements(policies),
         i,params,policy,validationFunc,failed,y;
     
     for (i = 0; i < policies.length; i++) {
@@ -460,7 +520,7 @@ function validate(policies, fullObject, propName, propValue, retArray) {
             propValue = openidm.decrypt(propValue);
         }
             
-        failed = validationFunc.call(this, fullObject, propValue, params, propName);
+        failed = validationFunc.call({ "failedPolicyRequirements": policyRequirements, "allPolicyRequirements": allPolicyRequirements }, fullObject, propValue, params, propName);
         if (failed.length > 0) {
             for ( y = 0; y < failed.length; y++) {
                 policyRequirements.push(failed[y]);
