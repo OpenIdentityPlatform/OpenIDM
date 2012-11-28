7315e08887ac6ff81ef4e76439b74df9a901694e
Andi Egloff <andi.egloff@forgerock.com>
2012-11-28 03:09:53 +0000

    OPENIDM-441 OPENIDM-947 [CR-964] Support consistent query definitions for prepared and regular OrientDB statements, support consistent contract for query on a (json pointer declared) property

---
+++ openidm-repo-orientdb/src/main/java/org/forgerock/openidm/repo/orientdb/impl/query/PrepareNotSupported.java
@@ -0,0 +1,63 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2012 ForgeRock AS. All Rights Reserved
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://forgerock.org/license/CDDLv1.0.html
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at http://forgerock.org/license/CDDLv1.0.html
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ */
+package org.forgerock.openidm.repo.orientdb.impl.query;
+
+/**
+ * An exception to indicate that a statement can not be converted into 
+ * a prepared statement 
+ *
+ * @author aegloff
+ */
+public class PrepareNotSupported extends Exception {
+
+    /** Serializable class a version number. */
+    static final long serialVersionUID = 1L;
+
+    /**
+     * Constructs a new exception with {@code null} as its detail message.
+     */
+    public PrepareNotSupported() {
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message.
+     */
+    public PrepareNotSupported(String message) {
+        super(message);
+    }
+    
+    /**
+     * Constructs a new exception with the specified cause.
+     */
+    public PrepareNotSupported(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message and cause.
+     */
+    public PrepareNotSupported(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
