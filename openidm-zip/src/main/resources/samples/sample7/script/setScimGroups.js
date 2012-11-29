0263a179e9b54f90647137d816c54b455adaf122
Matthias Tristl <matthias.tristl@forgerock.com>
2012-11-29 19:03:37 +0000

    Removed some spanish non ascii characters from the scripts

--- openidm-zip/src/main/resources/samples/sample7/script/setScimGroups.js
+++ openidm-zip/src/main/resources/samples/sample7/script/setScimGroups.js
@@ -2,7 +2,7 @@ var groupsList = source.groups;
 var groups = [];
 var i = 0;
 for (i = 0; i < groupsList.length; i += 1) {
-    var groupArray = groupsList[i].split("|");Ê
+    var groupArray = groupsList[i].split("|");
     groups.push({value: groupArray[0], display : groupArray[1]});
 }
 groups;
