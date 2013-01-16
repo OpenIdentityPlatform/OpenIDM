3bc32e3144be012d32a0d7021d159780bc4766ec
Jake Feasel <jake.feasel@forgerock.com>
2013-01-16 22:55:17 +0000

    Start of AM Proxy endpoint

--- openidm-zip/src/main/resources/samples/openam/script/amproxy.js
+++ openidm-zip/src/main/resources/samples/openam/script/amproxy.js
@@ -1,15 +1,32 @@
 
+/*global _, JSON */
+
+load("samples/openam/script/libs/json2.js");
+load("samples/openam/script/libs/underscore.js");
+
 java.lang.System.out.println(request);
-/*
-var openamServer = "http://jake.forgerock.com:8081",
+
+var openamServer = "jake.forgerock.com:8081",
+    passThroughURL = request.id.replace(/^amproxy/, ''),
     map = {
-      "_url" : openamServer + request.params.url,
-      "_method" : "post",
-      "_authenticate" : {"type": "basic", "user" : "kermit", "password" : "kermit"},
-      "_headers" : {"custom-key" : "sample value"},
-      "_body" : "{\"processDefinitionId\":\"EmailNotification:3:1073\",\"fromSender\":\"no-reply@activiti-integration.forgerock.com\",\"toEmail\":\"John.Doe@somecompany.com\"}"
-    };
- 
+      "_url" : "http://" + openamServer + passThroughURL,
+      "_method" : request.parent.method,
+      "_body": JSON.stringify(request.value),
+      "_content-type": "application/json"
+    },
+    urlArgs = [],
+    newHeaders = _.clone(request.parent.headers);
+
+_.each(request.params, function (value, key) {
+    urlArgs.push(encodeURIComponent(key) + "=" + encodeURIComponent(value));
+});
+newHeaders.Host = openamServer;
+map._url += "?" + urlArgs.join("&");
+map._headers = newHeaders;
+    
+java.lang.System.out.println(JSON.stringify(map));
+
 openidm.action("external/rest", map);
 
-*/
+
+//(function () { return request; }());
