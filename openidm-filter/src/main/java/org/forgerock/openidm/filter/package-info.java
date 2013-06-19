24532877d4d7debe452d4cd37bec9329d917f27b
Phill Cunnington <phill.cunnington@forgerock.com>
2013-06-19 12:31:54 +0000

    License header updates

--- openidm-filter/src/main/java/org/forgerock/openidm/filter/package-info.java
+++ openidm-filter/src/main/java/org/forgerock/openidm/filter/package-info.java
@@ -11,7 +11,7 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions Copyrighted [year] [name of copyright owner]".
  *
- * Copyright © 2012 ForgeRock AS. All rights reserved.
+ * Copyright © 2012-2013 ForgeRock Inc. All rights reserved.
  */
 
 /**
