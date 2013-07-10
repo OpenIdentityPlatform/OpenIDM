a178b7e2ac2e1de4f9b9b020b0b5564034ae5677
Brendan Mmiller <brendan.miller@forgerock.com>
2013-07-10 20:37:16 +0000

    OPENIDM-1326 Change contract with Javascript function to return nothing; throw exception if there is a problem.  This avoids a potential ClassCastException if the script returns something other than a boolean.  Moreover, there isn't a return payload in the SyncFailureHandler interface method anyway.

--- openidm-provisioner-openicf/src/main/java/org/forgerock/openidm/provisioner/openicf/syncfailure/ScriptedSyncFailureHandler.java
+++ openidm-provisioner-openicf/src/main/java/org/forgerock/openidm/provisioner/openicf/syncfailure/ScriptedSyncFailureHandler.java
@@ -77,17 +77,12 @@ public class ScriptedSyncFailureHandler implements SyncFailureHandler {
         scope.put("failedRecordUid", failedRecordUid);
 
         try {
-            boolean success = (Boolean) script.exec(scope);
-            logger.debug("sync failure script returned {}", success);
-            if (!success) {
-                throw new SyncHandlerException("sync failure script returned false for "
-                        + failedRecordUid + " object.");
-            }
-        } catch (Exception scriptEx) {
-            logger.debug("sync failure script on {} encountered exception", systemIdentifierName, scriptEx);
+            script.exec(scope);
+        } catch (Exception e) {
+            logger.debug("sync failure script on {} encountered exception", systemIdentifierName, e);
             throw new SyncHandlerException("Issue with handling the failure during synchronize "
                     + failedRecordUid + " object. " + exception.getMessage() + ". Failure handling reported "
-                    + scriptEx.getMessage(), scriptEx);
+                    + e.getMessage(), e);
         }
     }
 }
