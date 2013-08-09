c4d16c6942678434cbaca29ec60fc8b66bd0a23a
Chad Kienle <chad.kienle@forgerock.com>
2013-08-09 22:23:19 +0000

    package renaming to match trunk

--- openidm-security/src/main/java/org/forgerock/openidm/security/SecurityManager.java
+++ openidm-security/src/main/java/org/forgerock/openidm/security/SecurityManager.java
@@ -22,7 +22,7 @@
  * "Portions Copyrighted [year] [name of copyright owner]"
  */
 
-package org.forgerock.openidm.security.internal;
+package org.forgerock.openidm.security;
 
 import java.security.Security;
 
