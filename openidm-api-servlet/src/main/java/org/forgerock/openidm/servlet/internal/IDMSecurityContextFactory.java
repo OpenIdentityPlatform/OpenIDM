6b51a13cf6d285822b50122b5d67b22cb1d926b5
Brendan Mmiller <brendan.miller@forgerock.com>
2014-03-19 16:23:59 +0000

    OPENIDM-1697 (CR-3213) Fix regression in IDME-140 that validated non-empty roles in SecurityContext: instead, validate presence of field, but let be empty for auth to evaluate.

--- openidm-api-servlet/src/main/java/org/forgerock/openidm/servlet/internal/IDMSecurityContextFactory.java
+++ openidm-api-servlet/src/main/java/org/forgerock/openidm/servlet/internal/IDMSecurityContextFactory.java
@@ -98,7 +98,7 @@ public class IDMSecurityContextFactory implements HttpServletContextFactory {
         return !StringUtils.isEmpty(context.getAuthenticationId())
             && !StringUtils.isEmpty(context.getAuthorizationId().get(SecurityContext.AUTHZID_ID).toString())
             && !StringUtils.isEmpty(context.getAuthorizationId().get(SecurityContext.AUTHZID_COMPONENT).toString())
-            && !((List) context.getAuthorizationId().get(SecurityContext.AUTHZID_ROLES)).isEmpty();
+            && context.getAuthorizationId().get(SecurityContext.AUTHZID_ROLES) != null;
     }
 
     /**
