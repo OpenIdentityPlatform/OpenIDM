5d61ba69ae3dfb86d50b9bef3ebc980db658cc25
Jake Feasel <jake.feasel@forgerock.com>
2014-05-23 22:06:27 +0000

    CR-3554 - OPENIDM-1849 - MySQL stored procedures and allowed values for get-by-field-value

--- openidm-zip/src/main/resources/db/scripts/mysql/openidm.sql
+++ openidm-zip/src/main/resources/db/scripts/mysql/openidm.sql
@@ -328,6 +328,37 @@ ENGINE = InnoDB;
 
 
 
+delimiter //
+
+create procedure `openidm`.`byFieldValue` (t_schema varchar(255), t_name varchar(255), field varchar(255), val varchar(255), acceptable_fields varchar(255))
+begin
+    set @value = val;
+    select find_in_set(field, acceptable_fields) into @ok;
+    IF @ok != 0 THEN
+        set @query = concat('select * from ', t_schema, '.', t_name ,' where ', field, ' = ?');
+        prepare stmt from @query;
+        execute stmt using @value;
+    END IF;
+end //
+
+
+create procedure `openidm`.`getAllFromTable` (t_schema varchar(255), t_name varchar(255), order_by varchar(255), order_dir varchar(255), num_rows bigint, skip bigint, acceptable_order_by varchar(512))
+begin
+    set @num_rows = num_rows;
+    set @skip = skip;
+    select find_in_set(order_by, acceptable_order_by) into @order_by_ok;
+    select find_in_set(order_dir, 'asc,desc') into @order_dir_ok;
+    IF @order_by_ok != 0 && @order_dir_ok != 0 THEN
+        set @query = concat('select * from ', t_schema, '.', t_name ,' order by ', order_by ,' ', order_dir ,' limit ? offset ?');
+        prepare stmt from @query;
+        execute stmt using @num_rows, @skip;
+    END IF;
+end //
+
+delimiter ;
+
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
