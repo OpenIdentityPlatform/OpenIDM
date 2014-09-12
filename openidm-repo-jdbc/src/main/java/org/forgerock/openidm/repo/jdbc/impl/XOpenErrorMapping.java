f1b557fbc2156de5ab00dc219590714d2991137a
Brendan Mmiller <brendan.miller@forgerock.com>
2014-09-12 21:01:37 +0000

    OPENIDM-2190 (CR-4529) Fix Postgres detection of duplicate key insertion.

--- openidm-repo-jdbc/src/main/java/org/forgerock/openidm/repo/jdbc/impl/XOpenErrorMapping.java
+++ openidm-repo-jdbc/src/main/java/org/forgerock/openidm/repo/jdbc/impl/XOpenErrorMapping.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright © 2011 ForgeRock AS. All rights reserved.
+ * Copyright (c) 2011-2014 ForgeRock AS. All rights reserved.
  *
  * The contents of this file are subject to the terms
  * of the Common Development and Distribution License
@@ -24,8 +24,17 @@
 package org.forgerock.openidm.repo.jdbc.impl;
 
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
 
 import org.forgerock.openidm.repo.jdbc.ErrorType;
+
+import static org.forgerock.openidm.repo.jdbc.ErrorType.CONNECTION_FAILURE;
+import static org.forgerock.openidm.repo.jdbc.ErrorType.DEADLOCK_OR_TIMEOUT;
+import static org.forgerock.openidm.repo.jdbc.ErrorType.DUPLICATE_KEY;
+
 
 /**
  * Maps SQL state with Open Group (X/Open) SQL Standard codes
@@ -37,38 +46,58 @@ import org.forgerock.openidm.repo.jdbc.ErrorType;
  * @author aegloff
  */
 public class XOpenErrorMapping {
-    public static boolean isErrorType(SQLException ex, ErrorType errorType) {
-        boolean match = false;
-        String sqlState = ex.getSQLState(); // ISO/ANSI or Open Group (X/Open) SQL Standard codes
-        if (sqlState != null) {
-            sqlState = sqlState.trim();
-        }
-        switch (errorType) {
-            case CONNECTION_FAILURE: {
+
+    /** mapping of ErrorType constants to known X/Open SQL Standard SQL State codes */
+    private static final Map<ErrorType, List<String>> errorTypeToSqlStates =
+            new EnumMap<ErrorType, List<String>>(ErrorType.class);
+
+    static {
+        /* codes known to represent connection failure conditions */
+        errorTypeToSqlStates.put(CONNECTION_FAILURE, Arrays.asList(
                 // X/Open 08S01 is communication link failure
                 // Known to be retryable for MySQL
-                if ("08S01".equals(sqlState)) {
-                    match = true;
-                }
-                break;
-            }
-            case DUPLICATE_KEY: {
+                "08S01"
+        ));
+
+        /* codes known to represent duplicate key conditions */
+        errorTypeToSqlStates.put(DUPLICATE_KEY, Arrays.asList(
                 // X/Open 23000 is Integrity constraint violation
                 // Known to be used by Oracle, SQL Server, DB2, and MySQL
                 // Failure may be for a different constraint than duplicate though.
-                if ("23000".equals(sqlState)) {
-                    match = true;
-                }
-                break;
-            }
-            case DEADLOCK_OR_TIMEOUT: {
+                "23000",
+                // Postgres unique_violation per http://www.postgresql.org/docs/9.3/static/errcodes-appendix.html
+                // also Oracle per http://docs.oracle.com/javadb/10.3.3.0/ref/rrefexcept71493.html
+                "23505"
+        ));
+
+        /* codes known to represent deadlock/timeout conditions */
+        errorTypeToSqlStates.put(DEADLOCK_OR_TIMEOUT, Arrays.asList(
                 // X/Open 40001 is serialization failure such as timeout or deadlock
                 // Known to be retryable for MySQL
-                if ("40001".equals(sqlState)) {
-                    match = true;
-                }
-            }
+                "40001"
+        ));
+    }
+
+    /**
+     * Return whether the {@link SQLException} represents the given {@link ErrorType} using the
+     * exceptions SQL State.
+     *
+     * @param ex an SQLException
+     * @param errorType the error type to test
+     * @return whether the exception is of the error type given
+     */
+    public static boolean isErrorType(SQLException ex, ErrorType errorType) {
+        if (!errorTypeToSqlStates.containsKey(errorType)) {
+            // we have no known mapping for this error type
+            return false;
         }
-        return match;
+
+        // ISO/ANSI or Open Group (X/Open) SQL Standard codes
+        String sqlState = ex.getSQLState();
+        if (sqlState != null) {
+            sqlState = sqlState.trim();
+        }
+
+        return errorTypeToSqlStates.get(errorType).contains(sqlState);
     }
 }
