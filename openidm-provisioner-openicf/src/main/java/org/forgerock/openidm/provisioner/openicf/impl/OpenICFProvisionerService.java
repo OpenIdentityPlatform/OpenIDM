1a9fb0d39aa09be15f1e88ba9a34a8b864254011
Chad Kienle <chad.kienle@forgerock.com>
2014-09-26 17:37:33 +0000

    [OPENIDM-2263][CR-4657] Added null check on objectTypes in response to action=test request.

--- openidm-provisioner-openicf/src/main/java/org/forgerock/openidm/provisioner/openicf/impl/OpenICFProvisionerService.java
+++ openidm-provisioner-openicf/src/main/java/org/forgerock/openidm/provisioner/openicf/impl/OpenICFProvisionerService.java
@@ -1906,7 +1906,9 @@ public class OpenICFProvisionerService implements ProvisionerService, SingletonR
         jv.put("name", systemIdentifier.getName());
         jv.put("enabled", jsonConfiguration.get("enabled").defaultTo(Boolean.TRUE).asBoolean());
         jv.put("config", "config/provisioner.openicf/" + factoryPid);
-        jv.put("objectTypes", objectTypes.keySet().toArray());
+        if (objectTypes != null) {
+            jv.put("objectTypes", objectTypes.keySet().toArray());
+        }
         if (connectorReference != null) {
             jv.put(ConnectorUtil.OPENICF_CONNECTOR_REF, ConnectorUtil.getConnectorKey(connectorReference.getConnectorKey()));
         }
