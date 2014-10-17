9835e08035c227b0ec8035f0508ea48771f7c0d3
Elizabeth Browne <liz.browne@forgerock.com>
2014-10-17 14:30:55 +0000

    Bug fixes:
    CR-4871
    CR-4876

--- openidm-ui/openidm-ui-admin/src/main/js/org/forgerock/openidm/ui/admin/sync/SyncView.js
+++ openidm-ui/openidm-ui-admin/src/main/js/org/forgerock/openidm/ui/admin/sync/SyncView.js
@@ -68,49 +68,61 @@ define("org/forgerock/openidm/ui/admin/sync/SyncView", [
                 }
 
                 SchedulerDelegate.availableSchedules().then(_.bind(function (schedules) {
-                    _(schedules.result).each(function (scheduleId) {
-                        SchedulerDelegate.specificSchedule(scheduleId._id).then(_.bind(function (schedule) {
-                            // There is a liveSync Scheduler and it is enabled and the source matches the source of the mapping
-                            if (schedule.invokeService.indexOf("provisioner") >= 0 && schedule.enabled && schedule.invokeContext.source === this.mapping.source) {
-                                seconds = schedule.schedule.substr(schedule.schedule.indexOf("/") + 1);
-                                seconds = seconds.substr(0, seconds.indexOf("*") - 1);
+                    if (schedules.result.length > 0) {
+                        _(schedules.result).each(function (scheduleId) {
+                            SchedulerDelegate.specificSchedule(scheduleId._id).then(_.bind(function (schedule) {
+                                // There is a liveSync Scheduler and it is enabled and the source matches the source of the mapping
+                                if (schedule.invokeService.indexOf("provisioner") >= 0 && schedule.enabled && schedule.invokeContext.source === this.mapping.source) {
+                                    seconds = schedule.schedule.substr(schedule.schedule.indexOf("/") + 1);
+                                    seconds = seconds.substr(0, seconds.indexOf("*") - 1);
 
-                                this.$el.find(".noLiveSyncMessage").hide();
-                                this.$el.find(".systemObjectMessage").show();
-                                this.$el.find(".managedSourceMessage").hide();
-                                this.$el.find(".liveSyncSeconds").text(seconds);
-
-                                // This is a recon schedule
-                            } else if (schedule.invokeService.indexOf("sync") >= 0  ) {
-                                // The mapping is of a managed object
-                                if (this.mapping.source.indexOf("managed/") >= 0) {
                                     this.$el.find(".noLiveSyncMessage").hide();
-                                    this.$el.find(".systemObjectMessage").hide();
-                                    this.$el.find(".managedSourceMessage").show();
-                                } else {
-                                    this.$el.find(".noLiveSyncMessage").show();
-                                    this.$el.find(".systemObjectMessage").hide();
+                                    this.$el.find(".systemObjectMessage").show();
                                     this.$el.find(".managedSourceMessage").hide();
-                                }
+                                    this.$el.find(".liveSyncSeconds").text(seconds);
 
-                                if (schedule.invokeContext.mapping === this.mappingName) {
-                                    Scheduler.generateScheduler({
-                                        "element": $("#schedules"),
-                                        "defaults": {
-                                            enabled: schedule.enabled,
-                                            schedule: schedule.schedule,
-                                            persisted: schedule.persisted,
-                                            misfirePolicy: schedule.misfirePolicy
-                                        },
-                                        "onDelete": this.reconDeleted,
-                                        "invokeService": schedule.invokeService,
-                                        "scheduleId": scheduleId._id
-                                    });
-                                    this.$el.find("#addNew").hide();
+                                    // This is a recon schedule
+                                } else if (schedule.invokeService.indexOf("sync") >= 0) {
+                                    // The mapping is of a managed object
+                                    if (this.mapping.source.indexOf("managed/") >= 0) {
+                                        this.$el.find(".noLiveSyncMessage").hide();
+                                        this.$el.find(".systemObjectMessage").hide();
+                                        this.$el.find(".managedSourceMessage").show();
+                                    } else {
+                                        this.$el.find(".noLiveSyncMessage").show();
+                                        this.$el.find(".systemObjectMessage").hide();
+                                        this.$el.find(".managedSourceMessage").hide();
+                                    }
+
+                                    if (schedule.invokeContext.mapping === this.mappingName) {
+                                        Scheduler.generateScheduler({
+                                            "element": $("#schedules"),
+                                            "defaults": {
+                                                enabled: schedule.enabled,
+                                                schedule: schedule.schedule,
+                                                persisted: schedule.persisted,
+                                                misfirePolicy: schedule.misfirePolicy
+                                            },
+                                            "onDelete": this.reconDeleted,
+                                            "invokeService": schedule.invokeService,
+                                            "scheduleId": scheduleId._id
+                                        });
+                                        this.$el.find("#addNew").hide();
+                                    }
                                 }
-                            }
-                        }, this));
-                    }, this);
+                            }, this));
+                        }, this);
+
+                    } else if (this.mapping.source.indexOf("managed/") >= 0) {
+                        this.$el.find(".noLiveSyncMessage").hide();
+                        this.$el.find(".systemObjectMessage").hide();
+                        this.$el.find(".managedSourceMessage").show();
+                    } else {
+                        this.$el.find(".noLiveSyncMessage").show();
+                        this.$el.find(".systemObjectMessage").hide();
+                        this.$el.find(".managedSourceMessage").hide();
+                    }
+
                 }, this));
 
 
@@ -119,7 +131,7 @@ define("org/forgerock/openidm/ui/admin/sync/SyncView", [
                 }
 
                 this.setCurrentPolicyType();
-                
+
                 if(callback){
                     callback();
                 }
