108d473c8f313085fbd41495fc8a2545575f7053
Laurent Bristiel <laurent.bristiel@forgerock.com>
2014-11-17 13:52:47 +0000

    Default property is mail and no more email. Checked by Andi via Skype

--- openidm-zip/src/main/resources/samples/taskscanner/script/sunset.js
+++ openidm-zip/src/main/resources/samples/taskscanner/script/sunset.js
@@ -5,7 +5,7 @@
     // Create a change patch
     var patch = [{ "operation" : "replace", "field" : "active", "value" : false },{ "operation" : "replace", "field" : "accountStatus", "value" : "inactive" }];
     
-    logger.debug("Performing Sunset Task on {} ({})", input.email, objectID);
+    logger.debug("Performing Sunset Task on {} ({})", input.mail, objectID);
 
     // Perform update via patch so that we can do so regardless of revision change
     // NOTE: If we were to use update and the object had been modified during script execution
