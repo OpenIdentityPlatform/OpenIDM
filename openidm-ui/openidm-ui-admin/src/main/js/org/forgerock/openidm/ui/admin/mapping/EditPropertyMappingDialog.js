bcf3762009fabeec41288a06e5cef38a0b6c11e5
Jake Feasel <jake.feasel@forgerock.com>
2014-11-21 03:08:50 +0000

    OPENIDM-2592 - Recon would fail when configured mapping property using transform w/o source specified
    reviewed in chat

--- openidm-ui/openidm-ui-admin/src/main/js/org/forgerock/openidm/ui/admin/mapping/EditPropertyMappingDialog.js
+++ openidm-ui/openidm-ui-admin/src/main/js/org/forgerock/openidm/ui/admin/mapping/EditPropertyMappingDialog.js
@@ -204,7 +204,7 @@ define("org/forgerock/openidm/ui/admin/mapping/EditPropertyMappingDialog", [
                     .push(propertyObj);
             }
 
-            propertyObj.source = formContent.source;
+            propertyObj.source = formContent.source || "";
 
             if(this.transform_script_editor !== undefined) {
                 propertyObj.transform = this.transform_script_editor.generateScript();
