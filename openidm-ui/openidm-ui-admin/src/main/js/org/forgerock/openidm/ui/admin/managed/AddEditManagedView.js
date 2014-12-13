d621f27f4891f5ae44c3aa8ff41bd28582f1b115
Elizabeth Browne <liz.browne@forgerock.com>
2014-12-13 07:11:25 +0000

    Create a ui for editing/viewing/adding Managed Schema in adminUI CR-5682 OPENIDM-2645

--- openidm-ui/openidm-ui-admin/src/main/js/org/forgerock/openidm/ui/admin/managed/AddEditManagedView.js
+++ openidm-ui/openidm-ui-admin/src/main/js/org/forgerock/openidm/ui/admin/managed/AddEditManagedView.js
@@ -22,7 +22,7 @@
  * "Portions Copyrighted [year] [name of copyright owner]"
  */
 
-/*global define, $, _, Handlebars, form2js */
+/*global define, $, _, Handlebars, form2js, JSONEditor */
 
 define("org/forgerock/openidm/ui/admin/managed/AddEditManagedView", [
     "org/forgerock/openidm/ui/admin/util/AdminAbstractView",
@@ -130,6 +130,386 @@ define("org/forgerock/openidm/ui/admin/managed/AddEditManagedView", [
             }, this));
         },
 
+        loadSchema: function() {
+            // Set JSONEditor defaults
+            _(JSONEditor.defaults.options).extend({
+                disable_edit_json: true,
+                disable_array_reorder: false,
+                disable_collapse: true,
+                disable_properties: true,
+                show_errors: 'always',
+                template: 'handlebars',
+                theme: 'jqueryui',
+                no_additional_properties: true,
+                additionalItems: false,
+                required_by_default: true
+            });
+
+            // This function needs to be re-written for the ability to set the value of oneOf type
+            // enumerations.  Before "type" would take a value and never set the title(dropdown)
+            // Now the setValue val is an object containing a value and a title.
+
+            // This function needs to be restored for other instances of JSONEditor after load.
+
+            this.data.setValue = _.clone(JSONEditor.defaults.editors.multiple.prototype.setValue);
+
+            JSONEditor.defaults.editors.multiple.prototype.setValue = function(val,initial) {
+                if (_.isObject(val) && !_.isNull(val)) {
+                    this.switcher.value = val.display;
+                    this.type = _.indexOf(this.display_text, val.display);
+                    this.switchEditor(this.type);
+                    this.editors[this.type].setValue(val.val, initial);
+                    this.refreshValue();
+                }
+            };
+
+            this.data.managedObjectSchema = new JSONEditor(this.$el.find(".schemaEditor")[0], {
+                schema: {
+                    "title": "Managed Object",
+                    "type": "object",
+                    "headerTemplate": "{{self.objTitle}}",
+                    "definitions": {
+                        "oneOfTypes": {
+                            "oneOf": [
+                                {
+                                    "title": "String",
+                                    "type": "string",
+                                    "format": "hidden"
+                                }, {
+                                    "title": "Array",
+                                    "type": "object",
+                                    "properties": {
+                                        "itemType": {
+                                            "title":"Item Type",
+                                            "$ref": "#/definitions/oneOfTypes"
+                                        }
+                                    }
+                                }, {
+                                    "title": "Boolean",
+                                    "type": "string",
+                                    "displayType" : "Boolean",
+                                    "format": "hidden"
+                                }, {
+                                    "title": "Integer",
+                                    "type": "string",
+                                    "format": "hidden"
+                                }, {
+                                    "title": "Number",
+                                    "type": "string",
+                                    "format": "hidden"
+                                },{
+                                    "title": "Object",
+                                    "$ref": "#/definitions/managedObject"
+                                }, {
+                                    "title": "Resource Collection",
+                                    "type": "object",
+                                    "properties": {
+                                        "path": {
+                                            "type": "string",
+                                            "title": "Path"
+                                        },
+                                        "query": {
+                                            "type": "object",
+                                            "title": "Query",
+                                            "properties": {
+                                                "queryFilter": {
+                                                    "title": "Query Filter",
+                                                    "type": "string"
+                                                },
+                                                "fields": {
+                                                    "title": "Fields",
+                                                    "type": "array",
+                                                    "format": "table",
+                                                    "items": {
+                                                        "title": "Field",
+                                                        "type": "string"
+                                                    }
+                                                },
+                                                "sortKeys": {
+                                                    "title": "Sort Keys",
+                                                    "type": "array",
+                                                    "format": "table",
+                                                    "items": {
+                                                        "title": "Sort Key",
+                                                        "type": "string"
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            ]
+                        },
+                        "managedObject": {
+                            "type": "array",
+                            "format": "tabs",
+                            "items": {
+                                "type": "object",
+                                "title": "Property",
+                                "headerTemplate": "{{self.propertyName}}",
+                                "properties": {
+                                    "propertyName": {
+                                        "title": "Property Name",
+                                        "type": "string"
+                                    },
+                                    "title": {
+                                        "title": "Readable Title",
+                                        "type": "string"
+                                    },
+                                    "description": {
+                                        "title": "Description",
+                                        "type": "string",
+                                        "format": "textarea"
+                                    },
+                                    "viewable": {
+                                        "title": "Viewable",
+                                        "type": "boolean",
+                                        "required": true,
+                                        "default": false
+                                    },
+                                    "searchable": {
+                                        "title": "Searchable",
+                                        "type": "boolean",
+                                        "required": true,
+                                        "default": false
+                                    },
+                                    "required": {
+                                        "title": "Required",
+                                        "type": "boolean",
+                                        "required": true,
+                                        "default": false
+                                    },
+                                    "type": {
+                                        "title": "Type",
+                                        "$ref": "#/definitions/oneOfTypes"
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    "properties": {
+                        "title": {
+                            "title": "Readable Title",
+                            "type": "string"
+                        },
+                        "description": {
+                            "title": "Description",
+                            "type": "string"
+                        },
+                        "properties": {
+                            "title": "Properties",
+                            "$ref": "#/definitions/managedObject"
+                        }
+                    }
+                }
+
+            });
+
+            this.data.managedObjectSchema.on("change", _.bind(function () {
+                $(".schemaEditor input[type='hidden']").parent().hide();
+            }, this));
+
+            this.setDefaultData();
+
+            JSONEditor.defaults.editors.multiple.prototype.setValue = this.data.setValue;
+        },
+
+        setDefaultData: function() {
+            var managedSchema = this.data.currentManagedObject.schema,
+                jsonSchema = {};
+
+            if (managedSchema) {
+                jsonSchema = {
+                    title: managedSchema.title,
+                    description:  managedSchema.description,
+                    properties: []
+                };
+
+                jsonSchema.properties = this.translateSubProperties(managedSchema.properties, managedSchema.order, managedSchema.required);
+            }
+
+            this.data.managedObjectSchema.setValue(jsonSchema);
+        },
+
+        translateSubProperties: function(properties, order, required, forArray) {
+            var jsonEditorProperties = [];
+
+            _.each(order, function(propertyName) {
+                jsonEditorProperties.push(this.getType(properties[propertyName], propertyName, required, forArray));
+            }, this);
+
+            return jsonEditorProperties;
+        },
+
+        getType: function(property, name, required, forArray) {
+            var tempProperty = {},
+                toModify;
+
+            _.extend(tempProperty, _.omit(property, ["type", "order", "required", "items", "resourceCollection", "properties"]));
+
+            // Copy over object Key as a property
+            tempProperty.propertyName = name;
+            tempProperty.required = _.indexOf(required, name) >= 0;
+
+            // Copy over advanced properties
+            switch (property.type) {
+                case "boolean":
+                case "integer":
+                case "number":
+                    if (forArray) {
+                        tempProperty.itemType = {
+                            "val": property.type,
+                            "display": this.toProperCase(property.type)
+                        };
+                    } else {
+                        tempProperty.type = {
+                            "val": property.type,
+                            "display": this.toProperCase(property.type)
+                        };
+                    }
+                    break;
+
+                case "string":
+                    if (_.has(property, "resourceCollection")) {
+                        if (forArray) {
+                            tempProperty.itemType = {
+                                "display": "Resource Collection",
+                                "val": property.resourceCollection
+                            };
+                        } else {
+                            tempProperty.type = {
+                                "display": "Resource Collection",
+                                "val": property.resourceCollection
+                            };
+                        }
+                    } else {
+                        tempProperty.type = {
+                            "val": property.type,
+                            "display": this.toProperCase(property.type)
+                        };
+                    }
+                    break;
+
+                case "array":
+                    if (forArray) {
+                        toModify = tempProperty.itemType = {
+                            "val": {},
+                            "display": "Array"
+                        };
+                    } else {
+                        toModify = tempProperty.type = {
+                            "val": {},
+                            "display": "Array"
+                        };
+                    }
+
+                    if (_.has(property, "items")) {
+                        toModify.val  = this.getType(property.items, null, null, true);
+                    }
+                    break;
+
+                case "object":
+                    if (forArray) {
+                        tempProperty.itemType = {
+                            "val": this.translateSubProperties(property.properties, property.order, property.required, false),
+                            "display": this.toProperCase(property.type)
+                        };
+
+                        forArray = false;
+                    } else {
+                        tempProperty.type = {
+                            "val": this.translateSubProperties(property.properties, property.order, property.required, false),
+                            "display": this.toProperCase(property.type)
+                        };
+                    }
+                    break;
+            }
+
+            return tempProperty;
+        },
+
+        toProperCase: function(toConvert) {
+            return toConvert.charAt(0).toUpperCase() + toConvert.slice(1);
+        },
+
+        /**
+         * Recursively retrieves the type of a value
+         *
+         * @param node (root.properties.0.type or root.properties.0.type.itemType)
+         * @param property (schema.properties.myPropertyName or schema.properties.myPropertyName.items
+         * @param value - If this is an object leaf node or resource collection there is a value to provide
+         * @param editor
+         */
+        addType: function(node, property, value) {
+            var nodeSelect = $("[data-schemapath='"+node+"'] select:first"),
+                nodeType =  nodeSelect.val();
+
+            switch (nodeType) {
+                case "String":
+                case "Boolean":
+                case "Integer":
+                case "Number":
+                    property.type = nodeType.toLowerCase();
+                    break;
+
+                case "Array":
+                    property.type = "array";
+                    property.items = {};
+                    this.addType(node + ".itemType", property.items, this.data.managedObjectSchema.editors[node + ".itemType"].value);
+                    break;
+
+                case "Object":
+                    property.type = "object";
+                    _.extend(property, this.getObjectProperties(value, node));
+                    break;
+
+                case "Resource Collection":
+                    property.resourceCollection = value;
+                    property.type = "string";
+                    break;
+            }
+        },
+
+        getObjectProperties: function(props, node) {
+            var data = {
+                "properties": {},
+                "required": [],
+                "order": []
+            };
+            _.each(props, function(property, index) {
+                data.properties[property.propertyName] = {
+                    "description": property.description,
+                    "title": property.title,
+                    "viewable": property.viewable,
+                    "searchable": property.searchable
+                };
+
+                if (!node) {
+                    this.addType("root.properties."+ index +".type", data.properties[property.propertyName], property.type);
+                } else {
+                    this.addType(node + "." + index + ".type", data.properties[property.propertyName], property.type);
+                }
+
+
+                if (property.required) {
+                    data.required.push(property.propertyName);
+                }
+
+                data.order.push(property.propertyName);
+            }, this);
+
+            return data;
+        },
+
+        getManagedSchema: function() {
+            return _.extend({
+                "$schema": "http://forgerock.org/json-schema#",
+                "type": "object",
+                "title": this.data.managedObjectSchema.getValue().title,
+                "description": this.data.managedObjectSchema.getValue().description
+            }, this.getObjectProperties(this.data.managedObjectSchema.getValue().properties));
+        },
+
         managedRender: function(callback) {
             var tempEvent,
                 scriptOption,
@@ -140,6 +520,8 @@ define("org/forgerock/openidm/ui/admin/managed/AddEditManagedView", [
                 validatorsManager.validateAllFields(this.$el);
 
                 this.propertiesCounter = this.$el.find(".add-remove-block").length;
+
+                this.loadSchema();
 
                 _.each(this.$el.find("#mainHooksBody .managed-event-hook"), function(eventHook){
                     tempEvent = $(eventHook).attr("data-script-type");
@@ -255,8 +637,10 @@ define("org/forgerock/openidm/ui/admin/managed/AddEditManagedView", [
         formSubmit: function(event) {
             event.preventDefault();
 
-            var managedObject = this.setManagedObject(form2js('managedObjectForm', '.', true)),
+            var managedObject = this.setManagedObject(form2js('managedForm2JS', '.', true)),
                 nameCheck;
+
+            managedObject.schema = this.getManagedSchema();
 
             if(this.data.addState) {
                 nameCheck = this.checkManagedName(managedObject.name);
