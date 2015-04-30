20d17734dab2fe5660bddacd6c2abb5305bf913a
Mark Offutt <mark.offutt@forgerock.com>
2015-04-30 21:23:13 +0000

    OPENIDM-2866 Windows service created with launcher.bat has incorrect parameters

--- openidm-zip/src/main/resources/bin/install-service.bat
+++ openidm-zip/src/main/resources/bin/install-service.bat
@@ -1,4 +1,4 @@
-rem @echo off
+@echo off
 set "EXECUTABLE=%cd%\launcher.bat"
 
 rem Check that target executable exists
@@ -22,11 +22,15 @@ if not "%OPENIDM_OPTS%" == "" goto noOpenIDMOpts
 set OPENIDM_OPTS=${openidm.options} -Dfile.encoding=UTF-8
 :noOpenIDMOpts
 
+rem Make sure to remove any spaces and replace with semi-colon
+set OPENIDM_OPTS_SERVICE=%OPENIDM_OPTS: =;%
+
 rem set SERVER_START_PARAMS="-c;bin/launcher.json"
 set CP=bin/launcher.jar;bin/felix.jar
-set JAVA_OPTS_SERVICE=%OPENIDM_OPTS%-Djava.util.logging.config.file=conf\logging.properties;-Dlogback.configurationFile=conf\logging-config.xml;
+rem JAVA_OPTS_SERVICE will be fed to the prunmgr.exe which requires all semi-colon delimiters
+set JAVA_OPTS_SERVICE=%OPENIDM_OPTS_SERVICE%;-Djava.util.logging.config.file=conf\logging.properties;-Dlogback.configurationFile=conf\logging-config.xml;
 rem Enable debugging uncomment the line below
-set JAVA_OPTS_SERVICE=%JAVA_OPTS_SERVICE%-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005;
+rem set JAVA_OPTS_SERVICE=%JAVA_OPTS_SERVICE%;-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005;
 
 call "%EXECUTABLE%" /install %CMD_LINE_ARGS%
 goto :EOF
