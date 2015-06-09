f6e1e05b9b8dcc27546f00844ed4f84c46f070b4
Jon Branch <jon.branch@forgerock.com>
2015-06-09 18:43:43 +0000

    OPENIDM-3378 CR-7165 Prevent module availability errors during shutdown

--- openidm-infoservice/src/main/java/org/forgerock/openidm/info/impl/HealthService.java
+++ openidm-infoservice/src/main/java/org/forgerock/openidm/info/impl/HealthService.java
@@ -497,8 +497,8 @@ public class HealthService
         ServiceReference[] refs = null;
         try {
             refs = context.getBundleContext().getAllServiceReferences(null, null);
-        } catch (InvalidSyntaxException e) {
-            // Since we are not passing a filter this should not happen
+        } catch (Exception e) {
+            // Bundles and context may be in flux during shutdown
             logger.debug("Unexpected failure in getting service references", e);
         }
 
@@ -506,12 +506,14 @@ public class HealthService
         // required services.  Required services can be expressed as a regex,
         // for example: "org.forgerock.openidm.bootrepo.(orientdb|jdbc)"
         List<String> missingServices = new ArrayList<String>(requiredServices);
-        for (String req : requiredServices) {
-            for (ServiceReference ref : refs){
-                String pid = (String)ref.getProperty(Constants.SERVICE_PID);
-                if (pid !=null && pid.matches(req)) {
-                    missingServices.remove(req);
-                    break;
+        if (refs != null && refs.length > 0) {
+            for (String req : requiredServices) {
+                for (ServiceReference ref : refs) {
+                    String pid = (String) ref.getProperty(Constants.SERVICE_PID);
+                    if (pid != null && pid.matches(req)) {
+                        missingServices.remove(req);
+                        break;
+                    }
                 }
             }
         }
@@ -623,7 +625,7 @@ public class HealthService
         case Bundle.UNINSTALLED:
             return "UNINSTALLED ";
         }
-        return "UNKNDWN";
+        return "UNKNOWN";
     }
 
     /**
@@ -696,8 +698,8 @@ public class HealthService
 
         protected boolean isSameState(AppState compareState, String compareShortDesc) {
             return state == compareState
-                    && ((shortDesc == null && compareShortDesc == null) || shortDesc
-                            .equals(compareShortDesc));
+                    && ((shortDesc == null && compareShortDesc == null)
+                        || (shortDesc != null && shortDesc.equals(compareShortDesc)));
         }
 
         protected JsonValue toJsonValue() {
