b5d8921f7c55717029f0239dde95dbb366ad775a
Jake Feasel <jake.feasel@forgerock.com>
2015-07-31 14:05:44 -0700

    Adds java version check to startup.sh (1.7+)

--- openidm-zip/src/main/resources/startup.sh
+++ openidm-zip/src/main/resources/startup.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
-# 
+#
 # Copyright 2013 ForgeRock, Inc.
 #
 # The contents of this file are subject to the terms of the Common Development and
-# Distribution License (the License). You may not use this file except in compliance 
+# Distribution License (the License). You may not use this file except in compliance
 # with the License.
 #
-# You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for 
+# You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for
 # the specific language governing permission and limitations under the License.
 #
-# When distributing Covered Software, include this CDDL Header Notice in each file 
-# and include the License file at legal/CDDLv1.0.txt. If applicable, add the 
-# following below the CDDL Header, with the fields enclosed by brackets [] 
-# replaced by your own identifying information: 
+# When distributing Covered Software, include this CDDL Header Notice in each file
+# and include the License file at legal/CDDLv1.0.txt. If applicable, add the
+# following below the CDDL Header, with the fields enclosed by brackets []
+# replaced by your own identifying information:
 # "Portions copyright [year] [name of copyright owner]".
 #
+
+JAVA_VER=$(java -version 2>&1 | sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
+[ "$JAVA_VER" -ge 17 ] || (echo "Java version 1.7 and higher required" && exit 1)
 
 # clean up left over pid files if necessary
 cleanupPidFile() {
