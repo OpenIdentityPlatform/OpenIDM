9552adf5f8a4b459215c65dde6b5ede78de9d9a5
Jason Vincent <jason.vincent@forgerock.com>
2015-08-07 14:54:04 -0700

    OPENIDM-3739: correctly assign reconId and entryType on recon audit

--- openidm-core/src/main/java/org/forgerock/openidm/sync/impl/ObjectMapping.java
+++ openidm-core/src/main/java/org/forgerock/openidm/sync/impl/ObjectMapping.java
@@ -1169,6 +1169,7 @@ class ObjectMapping {
                     }
                     auditEvent.setStatus(status);
                     auditEvent.setAmbiguousTargetIds(op.getAmbiguousTargetIds());
+                    auditEvent.setReconId(reconContext.getReconId());
                     logEntry(auditEvent);
                 }
             }
@@ -1224,6 +1225,7 @@ class ObjectMapping {
                                 LazyObjectAccessor.qualifiedId(sourceObjectSet, op.getSourceObjectId()));
                     }
                     event.setStatus(status);
+                    event.setReconId(reconContext.getReconId());
                     logEntry(event);
                 }
             }
@@ -1372,8 +1374,10 @@ class ObjectMapping {
             String loggerMessage) throws SynchronizationException {
 
         ReconAuditEventLogger reconAuditEvent = new ReconAuditEventLogger(null, name, rootContext);
+        reconAuditEvent.setEntryType(AuditConstants.RECON_LOG_ENTRY_TYPE_RECON_END);
         reconAuditEvent.setReconciliationServiceReconAction(reconContext.getReconAction());
         reconAuditEvent.setStatus(status);
+        reconAuditEvent.setReconId(reconContext.getReconId());
         String simpleSummary = reconContext.getStatistics().simpleSummary();
         reconAuditEvent.setMessage(simpleSummary);
         reconAuditEvent.setMessageDetail(json(reconContext.getSummary()));
