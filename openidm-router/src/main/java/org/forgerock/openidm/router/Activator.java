a2c342834662cd175bdc309ec66f9e35e7d04fd0
Jon Branch <jon.branch@forgerock.com>
2015-08-13 13:50:46 -0700

    OPENIDM-3773 Migrate openidm-router to CREST3

--- openidm-router/src/main/java/org/forgerock/openidm/router/Activator.java
+++ openidm-router/src/main/java/org/forgerock/openidm/router/Activator.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2013 ForgeRock AS. All Rights Reserved
+ * Copyright 2013-2015 ForgeRock AS. All Rights Reserved
  *
  * The contents of this file are subject to the terms
  * of the Common Development and Distribution License
@@ -24,33 +24,22 @@
 
 package org.forgerock.openidm.router;
 
-import java.util.Dictionary;
-import java.util.Hashtable;
-
-import org.forgerock.json.resource.Connection;
-import org.forgerock.json.resource.ConnectionProvider;
-import org.forgerock.json.resource.PersistenceConfig;
 import org.forgerock.json.resource.RequestHandler;
-import org.forgerock.json.resource.ResourceException;
-import org.forgerock.json.resource.Resources;
 import org.forgerock.openidm.core.ServerConstants;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
 
-/**
- * A NAME does ...
- *
- */
-// TODO Delete this when openidm-core is migrated
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 public class Activator implements BundleActivator {
 
     private RouterRegistryImpl routerRegistry;
 
     private ServiceRegistration factoryServiceRegistration = null;
     private ServiceRegistration<RequestHandler> routerServiceRegistration = null;
-    private ServiceRegistration<PersistenceConfig> persistenceConfigRegistration = null;
 
     @Override
     public void start(BundleContext context) throws Exception {
@@ -70,34 +59,12 @@ public class Activator implements BundleActivator {
 
         routerServiceRegistration =
                 context.registerService(RequestHandler.class, routerRegistry.getInternalRouter(), properties);
-
-        final Connection connection = Resources.newInternalConnection(routerRegistry.getInternalRouter());
-
-        persistenceConfigRegistration =
-                context.registerService(PersistenceConfig.class, PersistenceConfig.builder()
-                        .connectionProvider(new ConnectionProvider() {
-                            @Override
-                            public Connection getConnection(String connectionId)
-                                    throws ResourceException {
-                                return connection;
-                            }
-
-                            @Override
-                            public String getConnectionId(Connection connection)
-                                    throws ResourceException {
-                                return "DEFAULT";
-                            }
-                        }).classLoader(this.getClass().getClassLoader()).build(), properties);
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
         if (null != routerRegistry) {
             routerRegistry.deactivate();
-        }
-        if (null != persistenceConfigRegistration) {
-            persistenceConfigRegistration.unregister();
-            persistenceConfigRegistration = null;
         }
         if (null != factoryServiceRegistration) {
             factoryServiceRegistration.unregister();
