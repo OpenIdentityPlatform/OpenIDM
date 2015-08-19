d2145921ac096542cea5c52680a4411a5f0f787a
Jason Vincent <jason.vincent@forgerock.com>
2015-08-19 13:03:10 -0700

    OPENIDM-3749: repaired AuditServiceImpl tests after crest3 migration.

--- openidm-audit/src/test/resources/container/logfilter.js
+++ openidm-audit/src/test/resources/container/logfilter.js
@@ -1,12 +1,26 @@
 (function() {
-    if (request.content.resourceOperation.operation.method === 'create') {
+    //TODO-crest3 note that the script dot notation no longer seems to work.
+
+    var method = request.content.asMap().get("resourceOperation").get("operation").get("method");
+    if (method == 'create') {
         // log creates
         return true;
-    } else if (request.content.resourceOperation.operation.method === 'update') {
+    } else if (method == 'update') {
         // do not log updates
         return false;
     } else {
         // log everything else
         return true;
     }
+
+    //if (request.content.resourceOperation.operation.method === 'create') {
+    //    // log creates
+    //    return true;
+    //} else if (request.content.resourceOperation.operation.method === 'update') {
+    //    // do not log updates
+    //    return false;
+    //} else {
+    //    // log everything else
+    //    return true;
+    //}
 }());
