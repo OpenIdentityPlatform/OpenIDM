84270ec090cce6f09ca47116c5e2cdf7822a4ac0
Jason Vincent <jason.vincent@forgerock.com>
2015-10-16 10:10:21 -0700

    OPENIDM-4082: ScriptHandler modified to pull ID from script result of present.

--- openidm-script/src/main/java/org/forgerock/openidm/script/ScriptedRequestHandler.java
+++ openidm-script/src/main/java/org/forgerock/openidm/script/ScriptedRequestHandler.java
@@ -1,25 +1,17 @@
 /*
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
  *
- * Copyright (c) 2013 ForgeRock AS. All Rights Reserved
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
  *
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the License). You may not use this file except in
- * compliance with the License.
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
  *
- * You can obtain a copy of the License at
- * http://forgerock.org/license/CDDLv1.0.html
- * See the License for the specific language governing
- * permission and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL
- * Header Notice in each file and include the License file
- * at http://forgerock.org/license/CDDLv1.0.html
- * If applicable, add the following below the CDDL Header,
- * with the fields enclosed by brackets [] replaced by
- * your own identifying information:
- * "Portions Copyrighted [year] [name of copyright owner]"
+ * Copyright 2013-2015 ForgeRock AS.
  */
 
 package org.forgerock.openidm.script;
@@ -423,14 +415,18 @@ public class ScriptedRequestHandler implements Scope, RequestHandler {
     private Promise<ResourceResponse, ResourceException> evaluate(final Request request, final Script script)
             throws ScriptException {
         Object result = script.eval();
+        String resourcePath = request.getResourcePathObject().leaf();
         if (null == result) {
-            return newResourceResponse(request.getResourcePath(), null, new JsonValue(null)).asPromise();
-        } else if (result instanceof JsonValue) {
-            return newResourceResponse(request.getResourcePath(), null, (JsonValue) result).asPromise();
-        } else if (result instanceof Map) {
-            return newResourceResponse(request.getResourcePath(), null, new JsonValue(result)).asPromise();
-        } else {
-            return newResourceResponse(request.getResourcePath(), null, new JsonValue(result)).asPromise();
+            return newResourceResponse(resourcePath, null, new JsonValue(null)).asPromise();
         }
+        JsonValue resultJson = (result instanceof JsonValue)
+                ? (JsonValue) result
+                : new JsonValue(result);
+        // If the resultJson isn't able to provide an ID, then we default to the resourcePath.
+        String id = resultJson.get(ResourceResponse.FIELD_CONTENT_ID).asString();
+        if (null == id || "".equals(id)) {
+            id = resourcePath;
+        }
+        return newResourceResponse(id, null, resultJson).asPromise();
     }
 }
