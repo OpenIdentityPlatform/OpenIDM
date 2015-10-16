d37ddb6be498d0b250e04ef7c1bfef6c03130643
Jon Branch <jon.branch@forgerock.com>
2015-10-16 10:53:36 -0700

    OPENIDM-4320 - Add javadoc.

--- openidm-maintenance/src/main/java/org/forgerock/openidm/maintenance/upgrade/UpdateFileLogEntry.java
+++ openidm-maintenance/src/main/java/org/forgerock/openidm/maintenance/upgrade/UpdateFileLogEntry.java
@@ -31,46 +31,101 @@ public class UpdateFileLogEntry {
     private String backupFile;
     private String stockFile;
 
+    /**
+     * Return the file path for the updated file.
+     *
+     * @return file path
+     */
     public String getFilePath() {
         return filePath;
     }
 
+    /**
+     * Set the file path for the updated file.
+     *
+     * @param filePath path of the updated file.
+     * @return this
+     */
     public UpdateFileLogEntry setFilePath(String filePath) {
         this.filePath = filePath;
         return this;
     }
 
+    /**
+     * Return the state of the updated file prior to the update.
+     *
+     * @return state of the file prior to the update.
+     */
     public String getFileState() {
         return fileState;
     }
 
+    /**
+     * Set the state of the file prior to the update.
+     *
+     * @param fileState state of the file prior to the update.
+     * @return this
+     */
     public UpdateFileLogEntry setFileState(String fileState) {
         this.fileState = fileState;
         return this;
     }
 
+    /**
+     * Return the action taken in updating the file.
+     *
+     * @return action taken updating the file
+     */
     public String getActionTaken() {
         return actionTaken;
     }
 
+    /**
+     * Set the action taken in updating the file.
+     *
+     * @param actionTaken action taken in updating the file.
+     * @return this
+     */
     public UpdateFileLogEntry setActionTaken(String actionTaken) {
         this.actionTaken = actionTaken;
         return this;
     }
 
+    /**
+     * Return the location of the backup file, if any.
+     *
+     * @return backup file location.
+     */
     public String getBackupFile() {
         return backupFile;
     }
 
+    /**
+     * Set the location of the backup file.
+     *
+     * @param backupFile location of the backup file.
+     * @return this
+     */
     public UpdateFileLogEntry setBackupFile(String backupFile) {
         this.backupFile = backupFile;
         return this;
     }
 
+    /**
+     * Return the location of the stock file, if any.
+     *
+     * @return stock file location.
+     */
     public String getStockFile() {
         return stockFile;
     }
 
+    /**
+     * Set the location of the stock file.
+     *
+     * @param stockFile stock file location.
+     * @return this
+     */
     public UpdateFileLogEntry setStockFile(String stockFile) {
         this.stockFile = stockFile;
         return this;
