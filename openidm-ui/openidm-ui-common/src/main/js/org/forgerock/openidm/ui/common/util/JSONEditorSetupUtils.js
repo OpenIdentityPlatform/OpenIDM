d831a02394471cdc7111c67ebe5c0ed27b9b1219
Huck Elliott <huck.elliott@gmail.com>
2015-10-19 14:14:02 -0700

    OPENIDM-4213 adds support for relationship data type to the managed object schema editor. Adds the ability to handle Validation policies. Adds other fields that did not exist before (End users allowed to edit?,Minimum Length,Pattern,Is Virtual?)

--- openidm-ui/openidm-ui-common/src/main/js/org/forgerock/openidm/ui/common/util/JSONEditorSetupUtils.js
+++ openidm-ui/openidm-ui-common/src/main/js/org/forgerock/openidm/ui/common/util/JSONEditorSetupUtils.js
@@ -26,11 +26,42 @@
 
 define("org/forgerock/openidm/ui/common/util/JSONEditorSetupUtils", [
     "jquery",
-    "jsonEditor"
-], function ($, JSONEditor) {
+    "underscore",
+    "jsonEditor",
+    "org/forgerock/commons/ui/common/main/Router"
+], function ($, _, JSONEditor, router) {
     var JSONEditorPostBuild = JSONEditor.AbstractEditor.prototype.postBuild;
     JSONEditor.AbstractEditor.prototype.postBuild = function () {
-        var ret = JSONEditorPostBuild.apply(this, arguments);
+        var ret = JSONEditorPostBuild.apply(this, arguments),
+        
+            //showHideRelationshipFields shows and hides the correct properties in the managed schema editor
+            //when the "type" or "itemType" property is changed to/from relationship
+            showHideRelationshipFields = function (container, schemapath, type) {
+                var typeLength = type.length + 1,
+                    typeSelect;
+                
+                //any field that has a schemapath ending in "type" or "itemType" needs to have an onChange event attached to it
+                if (schemapath.substr(schemapath.length - typeLength) === "." + type) {
+                    typeSelect = container.parent().parent().find("select");
+                    
+                    typeSelect.change(function () {
+                        var arrayItemProps = $(this).parent().closest(".row").find(".well:first").find(".row:lt(2)");
+                        
+                        if ($(this).val() === "Relationship" && type === "type") {
+                            //this is not an array of relationships so hide relationship properties:
+                            //"Return by Default", "Reverse Relationship", and "Reverse Property Name"
+                            arrayItemProps.hide();
+                        } else {
+                            if ($(this).val() === "Relationship" && type === "itemType") {
+                                //this is an array of relationships so show
+                                //"Return by Default", "Reverse Relationship", and "Reverse Property Name"
+                                arrayItemProps.show();
+                            }
+                        }
+                    });
+                }
+            };
+        
         if (this.path && this.input && this.label && !this.input.id && !this.label.htmlFor) {
             this.input.id = (this.jsoneditor.options.uuid || this.jsoneditor.uuid) + "." + this.path;
             this.label.htmlFor = (this.jsoneditor.options.uuid || this.jsoneditor.uuid) + "." + this.path;
@@ -40,6 +71,13 @@ define("org/forgerock/openidm/ui/common/util/JSONEditorSetupUtils", [
                 $(this.label).addClass("col-sm-2");
                 $(this.input).wrap("<div class='col-sm-10'></div>");
             }
+            
+            //if this json editor is for the Managed Object Schema Editor handle relationship fields
+            if (_.contains(router.currentRoute.view,"AddEditManagedView")) {
+                showHideRelationshipFields($(this.container), $(this.container).data("schemapath"), "type");
+                showHideRelationshipFields($(this.container), $(this.container).data("schemapath"), "itemType");
+            }
+            
         }
 
         if (this.jsoneditor.options.disable_array_delete_all) {
