d406094e2233643f2ee35866ad13b4f859af1781
Alin Brici <alin.brici@forgerock.com>
2015-11-10 10:23:39 -0800

    Adding support for new OperationOptions from ICF 1.4
    adding ICF 1.5 supported OperationOptions

--- openidm-provisioner-openicf/src/main/java/org/forgerock/openidm/provisioner/openicf/commons/AttributeInfoHelper.java
+++ openidm-provisioner-openicf/src/main/java/org/forgerock/openidm/provisioner/openicf/commons/AttributeInfoHelper.java
@@ -53,6 +53,7 @@ import org.identityconnectors.framework.common.objects.OperationOptions;
 import org.identityconnectors.framework.common.objects.OperationOptionsBuilder;
 import org.identityconnectors.framework.common.objects.OperationalAttributes;
 import org.identityconnectors.framework.common.objects.QualifiedUid;
+import org.identityconnectors.framework.common.objects.SortKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -337,6 +338,18 @@ public class AttributeInfoHelper {
             builder.setRunWithPassword(getSingleValue(value, GuardedString.class));
         } else if (OperationOptions.OP_SCOPE.equals(name)) {
             builder.setScope(getSingleValue(value, String.class));
+        } else if (OperationOptions.OP_PAGED_RESULTS_COOKIE.equals(name)) {
+            builder.setPagedResultsCookie(getSingleValue(value, String.class));
+        } else if (OperationOptions.OP_PAGED_RESULTS_OFFSET.equals(name)) {
+            builder.setPagedResultsOffset(getSingleValue(value, Integer.class));
+        } else if (OperationOptions.OP_PAGE_SIZE.equals(name)) {
+            builder.setPageSize(getSingleValue(value, Integer.class));
+        } else if (OperationOptions.OP_FAIL_ON_ERROR.equals(name)) {
+            builder.setAttributesToGet(getSingleValue(value, Boolean.class).toString());
+        } else if (OperationOptions.OP_REQUIRE_SERIAL.equals(name)) {
+            builder.setAttributesToGet(getSingleValue(value, String.class));
+        } else if (OperationOptions.OP_SORT_KEYS.equals(name)) {
+            builder.setSortKeys((List<SortKey>) getMultiValue(value, SortKey.class));
         } else {
             builder.setOption(name, getNewValue(value == null ? defaultValue : value, attributeInfo
                     .isMultiValued(), attributeInfo.getType()));
