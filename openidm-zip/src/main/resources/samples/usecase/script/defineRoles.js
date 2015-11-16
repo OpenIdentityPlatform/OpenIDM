816a626185cf1d340eb86c906bb65f7d2acbbf57
jason.browne <jason.browne@forgerock.com>
2015-11-16 12:14:34 -0800

    OPENIDM-4602 - Fix incorrect semicolon issue

--- openidm-zip/src/main/resources/samples/usecase/script/defineRoles.js
+++ openidm-zip/src/main/resources/samples/usecase/script/defineRoles.js
@@ -1,13 +1,15 @@
 /*global source */
 
 (function () {
-    var roles = [ { 
-        "_ref" : "repo/internal/roles/openidm-authorized";
+    var roles = [ {
+        "_ref" : "repo/internal/roles/openidm-authorized"
     } ];
+
     if (source !== undefined && 'manager' === source) {
         roles.push({
-            "_ref" : "repo/internal/roles/openidm-admin";
+            "_ref" : "repo/internal/roles/openidm-admin"
         });
     }
+
     return roles;
 }());
