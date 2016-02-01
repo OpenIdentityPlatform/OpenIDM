eb49fcb10327ae5ca3ebd1067301a281b568f56a
Huck Elliott <huck.elliott@gmail.com>
2016-02-01 10:37:14 -0800

    OPENIDM-4957 fixes bug where changes to fields saved in ui-configuration for the OPENAM_SESSION module were not reflected in the module edit dialog when reopened after changes were made...this was accomplished by rearranging the flow of how these values are loaded into the dialog. Also moves the call to openam's info endpoint used to validate the openamDeploymentUrl to happen only before the final save of authentication.json and then only if the OPENAM_SESSION module is enabled

--- openidm-ui/openidm-ui-admin/src/main/js/org/forgerock/openidm/ui/admin/settings/authentication/AuthenticationView.js
+++ openidm-ui/openidm-ui-admin/src/main/js/org/forgerock/openidm/ui/admin/settings/authentication/AuthenticationView.js
@@ -22,13 +22,21 @@ define("org/forgerock/openidm/ui/admin/settings/authentication/AuthenticationVie
     "org/forgerock/openidm/ui/admin/settings/authentication/AuthenticationAbstractView",
     "org/forgerock/commons/ui/common/util/Constants",
     "org/forgerock/openidm/ui/admin/settings/authentication/SessionModuleView",
-    "org/forgerock/openidm/ui/admin/settings/authentication/AuthenticationModuleView"
+    "org/forgerock/openidm/ui/admin/settings/authentication/AuthenticationModuleView",
+    "org/forgerock/commons/ui/common/util/UIUtils",
+    "org/forgerock/openidm/ui/common/delegates/SiteConfigurationDelegate",
+    "org/forgerock/openidm/ui/common/delegates/ConfigDelegate",
+    "org/forgerock/openidm/ui/common/delegates/OpenAMProxyDelegate"
 
 ], function($, _,
             AuthenticationAbstractView,
             Constants,
             SessionModuleView,
-            AuthenticationModuleView) {
+            AuthenticationModuleView,
+            UIUtils,
+            SiteConfigurationDelegate,
+            ConfigDelegate,
+            OpenAMProxyDelegate) {
 
     var AuthenticationView = AuthenticationAbstractView.extend({
         template: "templates/admin/settings/AuthenticationTemplate.html",
@@ -62,15 +70,67 @@ define("org/forgerock/openidm/ui/admin/settings/authentication/AuthenticationVie
         },
 
         save: function (e) {
+            var doSave = _.bind(function (reload) {
+                    this.saveAuthentication().then(function() {
+                        if (!reload) {
+                            SessionModuleView.render();
+                            AuthenticationModuleView.render({
+                                "addedOpenAM": function () {
+                                    SessionModuleView.addedOpenAM();
+                                }
+                            });
+                        } else {
+                            /*when changes are made to the OPENAM_SESSION module the page needs to be reloaded so
+                              the changes to ui-configuration.json are seen immediately*/
+                            location.reload(true);
+                        }
+                    });
+                }, this);
             e.preventDefault();
-            this.saveAuthentication().then(function() {
-                SessionModuleView.render();
-                AuthenticationModuleView.render({
-                    "addedOpenAM": function () {
-                        SessionModuleView.addedOpenAM();
-                    }
+            
+            if(AuthenticationModuleView.model.amSettings) {
+                this.handleOpenAMUISettings(AuthenticationModuleView.model.amSettings).then(function () {
+                    doSave(true);
                 });
-            });
+            } else {
+                doSave();
+            }
+        },
+
+        handleOpenAMUISettings: function(amSettings){
+            var prom = $.Deferred(),
+                amAuthIndex = _.findIndex(AuthenticationModuleView.model.changes, { name: "OPENAM_SESSION" }),
+                confirmed = function(){
+                    SiteConfigurationDelegate.getConfiguration().then(function(uiConfig){
+                        ConfigDelegate.updateEntity("ui/configuration", { configuration: _.extend(uiConfig, amSettings) }).then(function() {
+                            prom.resolve();
+                        });
+                    });
+                };
+            
+            if (amSettings.openamAuthEnabled) {
+                // Validate openamDeploymentUrl
+                OpenAMProxyDelegate.serverinfo(amSettings.openamDeploymentUrl).then(_.bind(function(info){
+                        if (info.cookieName) {
+                            // Set openamSSOTokenCookieName for this module
+                            AuthenticationModuleView.model.changes[amAuthIndex].properties.openamSSOTokenCookieName = info.cookieName;
+                            confirmed();
+                        } else {
+                            UIUtils.jqConfirm($.t("templates.auth.openamDeploymentUrlConfirmation"), confirmed);
+                        }
+                    },this),
+                    _.bind(function(){
+                        UIUtils.jqConfirm($.t("templates.auth.openamDeploymentUrlConfirmation"), confirmed);
+                    },this)
+                );
+            } else {
+                confirmed();
+            }
+            
+            //remove this property so it doesn't get saved in ui-configuration.json
+            delete amSettings.openamDeploymentUrl;
+            
+            return prom;
         }
 
     });
