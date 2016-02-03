72b8f92dbb6b0f5f50ef8e75c63060783f271717
Jake Feasel <jake.feasel@forgerock.com>
2016-02-03 08:55:27 -0800

    OPENIDM-4954 - Changes to passed variables now trigger change events, and are saved

--- openidm-ui/openidm-ui-admin/src/test/nightwatchjs/data/devGlobals.js
+++ openidm-ui/openidm-ui-admin/src/test/nightwatchjs/data/devGlobals.js
@@ -1,14 +1,15 @@
 module.exports = {
+        baseUrl: "http://localhost:8080/admin/",
         login: {
             username: "openidm-admin",
             password: "openidm-admin",
-            url: "http:localhost:8080/admin/index.html#login/",
+            url: "#login/",
             helpers: {
                 login: function (client) {
                     var data = client.globals;
-                    
+
                     client
-                        .url(data.login.url)
+                        .url(data.baseUrl + data.login.url)
                         .waitForElementPresent('input[name=loginButton]', 2000)
                         .setValue('input[name=login]', data.login.username)
                         .setValue('input[name=password]', data.login.password)
