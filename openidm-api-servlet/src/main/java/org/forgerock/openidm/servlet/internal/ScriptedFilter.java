5dde5be110d0c6151ea7571d238f14c2a50ad774
Brendan Miller <brendan.miller@forgerock.com>
2016-02-19 15:17:55 -0800

    OPENIDM-5324 Improved javadoc on ScriptedFilter

--- openidm-api-servlet/src/main/java/org/forgerock/openidm/servlet/internal/ScriptedFilter.java
+++ openidm-api-servlet/src/main/java/org/forgerock/openidm/servlet/internal/ScriptedFilter.java
@@ -11,7 +11,7 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions copyright [year] [name of copyright owner]".
  *
- * Copyright 2013-2015 ForgeRock AS.
+ * Copyright 2013-2016 ForgeRock AS.
  */
 
 package org.forgerock.openidm.servlet.internal;
@@ -19,6 +19,7 @@ package org.forgerock.openidm.servlet.internal;
 import static org.forgerock.util.promise.Promises.newResultPromise;
 
 import org.apache.commons.lang3.tuple.Pair;
+import org.forgerock.script.ScriptName;
 import org.forgerock.services.context.Context;
 import org.forgerock.json.JsonPointer;
 import org.forgerock.json.resource.ActionRequest;
@@ -65,6 +66,13 @@ public class ScriptedFilter implements Filter {
     /** the onFailure script and the config path at which it is defined */
     private final Pair<JsonPointer, ScriptEntry> onFailure;
 
+    /**
+     * Construct a ScriptedFilter from scripts to execute on each request, response, or failure.
+     *
+     * @param onRequest the script to evaluate on a request
+     * @param onResponse the script to evaluate on a response
+     * @param onFailure the script to evaluate on a failuree
+     */
     public ScriptedFilter(
             Pair<JsonPointer, ScriptEntry> onRequest,
             Pair<JsonPointer, ScriptEntry> onResponse,
@@ -77,7 +85,7 @@ public class ScriptedFilter implements Filter {
     @Override
     public Promise<ActionResponse, ResourceException> filterAction(
             final Context context, final ActionRequest request, final RequestHandler next) {
-        return filterResourceResponseRequest(context, request,
+        return filterRequest(context, request,
                 new AsyncFunction<Request, ActionResponse, ResourceException>() {
                     @Override
                     public Promise<ActionResponse, ResourceException> apply(Request value) {
@@ -89,7 +97,7 @@ public class ScriptedFilter implements Filter {
     @Override
     public Promise<ResourceResponse, ResourceException> filterCreate(
             final Context context, final CreateRequest request, final RequestHandler next) {
-        return filterResourceResponseRequest(context, request,
+        return filterRequest(context, request,
                 new AsyncFunction<Request, ResourceResponse, ResourceException>() {
                     @Override
                     public Promise<ResourceResponse, ResourceException> apply(Request value) {
@@ -101,7 +109,7 @@ public class ScriptedFilter implements Filter {
     @Override
     public Promise<ResourceResponse, ResourceException> filterDelete(
             final Context context, final DeleteRequest request, final RequestHandler next) {
-        return filterResourceResponseRequest(context, request,
+        return filterRequest(context, request,
                 new AsyncFunction<Request, ResourceResponse, ResourceException>() {
                     @Override
                     public Promise<ResourceResponse, ResourceException> apply(Request value) {
@@ -113,7 +121,7 @@ public class ScriptedFilter implements Filter {
     @Override
     public Promise<ResourceResponse, ResourceException> filterPatch(
             final Context context, final PatchRequest request, final RequestHandler next) {
-        return filterResourceResponseRequest(context, request,
+        return filterRequest(context, request,
                 new AsyncFunction<Request, ResourceResponse, ResourceException>() {
                     @Override
                     public Promise<ResourceResponse, ResourceException> apply(Request value) {
@@ -125,7 +133,7 @@ public class ScriptedFilter implements Filter {
     @Override
     public Promise<ResourceResponse, ResourceException> filterRead(
             final Context context, final ReadRequest request, final RequestHandler next) {
-        return filterResourceResponseRequest(context, request,
+        return filterRequest(context, request,
                 new AsyncFunction<Request, ResourceResponse, ResourceException>() {
                     @Override
                     public Promise<ResourceResponse, ResourceException> apply(Request value) {
@@ -137,7 +145,7 @@ public class ScriptedFilter implements Filter {
     @Override
     public Promise<ResourceResponse, ResourceException> filterUpdate(
             final Context context, final UpdateRequest request, final RequestHandler next) {
-        return filterResourceResponseRequest(context, request,
+        return filterRequest(context, request,
                 new AsyncFunction<Request, ResourceResponse, ResourceException>() {
                     @Override
                     public Promise<ResourceResponse, ResourceException> apply(Request value) {
@@ -150,7 +158,7 @@ public class ScriptedFilter implements Filter {
     public Promise<QueryResponse, ResourceException> filterQuery(
             final Context context, final QueryRequest request, final QueryResourceHandler handler,
             final RequestHandler next) {
-        return filterResourceResponseRequest(context, request,
+        return filterRequest(context, request,
                 new AsyncFunction<Request, QueryResponse, ResourceException>() {
                     @Override
                     public Promise<QueryResponse, ResourceException> apply(Request value) throws ResourceException {
@@ -159,7 +167,22 @@ public class ScriptedFilter implements Filter {
                 });
     }
 
-    public <R extends Response> Promise<R, ResourceException> filterResourceResponseRequest(
+    /**
+     * Filter the request by:
+     * <ol>
+     *     <li>evaluating the onRequest script (if present)</li>
+     *     <li>handle the request</li>
+     *     <li>evaluate the onResponse script (if present), or</li>
+     *     <li>evaluate the onFaioure script (if present)</li>
+     * </ol>
+     *
+     * @param context the request context
+     * @param request the request
+     * @param handleRequest a Function that handles the CRUDPAQ request (via the RequestHandler passed to the filter)
+     * @param <R> the type of Response expected
+     * @return a Promise of either the Response from handler or an ResourceException if the request failed
+     */
+    private <R extends Response> Promise<R, ResourceException> filterRequest(
             final Context context,
             final Request request,
             final AsyncFunction<Request, R, ResourceException> handleRequest) {
@@ -187,7 +210,14 @@ public class ScriptedFilter implements Filter {
 
     // ----- Evaluate Script methods
 
-    protected Promise<Request, ResourceException> evaluateOnRequest(final Context context, final Request request) {
+    /**
+     * Evaluate the onRequest script, if present.  This is executed <em>before</em> the request is handled.
+     *
+     * @param context the Context for the request to be handled (used as a script binding)
+     * @param request the Request to be handled (used as a script binding)
+     * @return a Promise holding the Request to be handled
+     */
+    private Promise<Request, ResourceException> evaluateOnRequest(final Context context, final Request request) {
         if (onRequest != null) {
             ScriptEntry scriptEntry = onRequest.getRight();
             if (!scriptEntry.isActive()) {
@@ -197,19 +227,24 @@ public class ScriptedFilter implements Filter {
             }
             Script script = populateScript(scriptEntry, context, request);
             try {
-                script.eval();
-            } catch (Exception e) {
-                logger.debug("Filter/{} script {} encountered exception at {}",
-                        onRequest.getRight().getName(), onRequest.getLeft(), e);
-                ResourceException re = Utils.adapt(e);
-                logger.debug("ResourceException detail: " + re.getDetail());
-                return re.asPromise();
+                evalScript(script, onRequest.getLeft() , onRequest.getRight().getName());
+            } catch (ResourceException e) {
+                return e.asPromise();
             }
         }
         return newResultPromise(request);
     }
 
-    public <R extends Response> Promise<R, ResourceException> evaluateOnResponse(final Context context,
+    /**
+     * Evaluate the onResponse script, if present.
+     *
+     * @param context the Context for the request (used as a script binding)
+     * @param request the Request that failed (used as a script binding)
+     * @param response the response that resulted from the request (used as a script binding)
+     * @param <R> the type of Response being returned
+     * @return a Promise holding the ResourceException that was thrown
+     */
+    private <R extends Response> Promise<R, ResourceException> evaluateOnResponse(final Context context,
             final Request request, final R response) {
         if (onResponse != null) {
             logger.info("Filter response: {}.", context.getId());
@@ -222,19 +257,24 @@ public class ScriptedFilter implements Filter {
             Script script = populateScript(scriptEntry, context, request);
             script.put("response", response);
             try {
-                script.eval();
-            } catch (Exception e) {
-                logger.debug("Filter/{} script {} encountered exception at {}",
-                        onResponse.getRight().getName(), onResponse.getLeft(), e);
-                ResourceException re = Utils.adapt(e);
-                logger.debug("ResourceException detail: " + re.getDetail());
-                return re.asPromise();
+                evalScript(script, onResponse.getLeft(), onResponse.getRight().getName());
+            } catch (ResourceException e) {
+                return e.asPromise();
             }
         }
         return newResultPromise(response);
     }
 
-    public <R extends Response> Promise<R, ResourceException> evaluateOnFailure(final Context context,
+    /**
+     * Evaluate the onFailure script, if present.
+     *
+     * @param context the Context for the failed request (used as a script binding)
+     * @param request the Request that failed (used as a script binding)
+     * @param error the ResourceException (error) that occurred (used as a script binding)
+     * @param <R> the type of Response being returned
+     * @return a Promise holding the ResourceException that was thrown
+     */
+    private <R extends Response> Promise<R, ResourceException> evaluateOnFailure(final Context context,
             final Request request, final ResourceException error) {
         if (onFailure != null) {
             ScriptEntry scriptEntry = onFailure.getRight();
@@ -246,22 +286,45 @@ public class ScriptedFilter implements Filter {
             Script script = populateScript(scriptEntry, context, request);
             script.put("exception", error.includeCauseInJsonValue().toJsonValue().asMap());
             try {
-                script.eval();
-            } catch (Exception e) {
-                logger.debug("Filter/{} script {} encountered exception at {}",
-                        onFailure.getRight().getName(), onFailure.getLeft(), e);
-                ResourceException re = Utils.adapt(e);
-                logger.debug("ResourceException detail: " + re.getDetail());
-                return re.asPromise();
+                evalScript(script, onFailure.getLeft(), onFailure.getRight().getName());
+            } catch (ResourceException e) {
+                return e.asPromise();
             }
         }
         return error.asPromise();
     }
 
+    /**
+     * Populate the script with the context and request bindings.
+     *
+     * @param scriptEntry the ScriptEntry
+     * @param context the Context
+     * @param request the Request
+     * @return the populated Script
+     */
     private Script populateScript(final ScriptEntry scriptEntry, final Context context, final Request request) {
         final Script script = scriptEntry.getScript(context);
         script.put("request", request);
         script.put("context", context);
         return script;
+    }
+
+    /**
+     * Evaluate the script.
+     *
+     * @param script the Script to evaluate
+     * @param filterPath the filter path (for debug messages)
+     * @param scriptName the script name (for debug messages)
+     * @throws ResourceException on failure to execute the script
+     */
+    private void evalScript(Script script, JsonPointer filterPath, ScriptName scriptName) throws ResourceException {
+        try {
+            script.eval();
+        } catch (Exception e) {
+            logger.debug("Filter/{} script {} encountered exception at {}", filterPath, scriptName, e);
+            ResourceException re = Utils.adapt(e);
+            logger.debug("ResourceException detail: " + re.getDetail());
+            throw re;
+        }
     }
 }
