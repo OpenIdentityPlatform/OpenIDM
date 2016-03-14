8fdef63256620386c0cfe3f695eb9c646fe9e8d2
Brendan Miller <brendan.miller@forgerock.com>
2016-03-14 16:31:54 -0700

    OPENIDM-5422 Implement Javascript TestRunner environment

--- openidm-zip/src/test/java/org/forgerock/openidm/bin/defaults/script/AuthenticationJsonTest.java
+++ openidm-zip/src/test/java/org/forgerock/openidm/bin/defaults/script/AuthenticationJsonTest.java
@@ -1,4 +1,21 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2016 ForgeRock AS.
+ */
 package org.forgerock.openidm.bin.defaults.script;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.forgerock.json.JsonValue;
@@ -8,7 +25,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
-import static org.testng.Assert.assertEquals;
 
 /**
  * A unit test to test the raw key-value in Authentication.json file.
@@ -26,7 +42,7 @@ public class AuthenticationJsonTest {
                 .get("keyAlias")
                 .toString();
 
-        assertEquals(keyAliasValue, "\"&{openidm.https.keystore.cert.alias}\"");
+        assertThat(keyAliasValue).isEqualTo("\"&{openidm.https.keystore.cert.alias}\"");
     }
 
     // This method will be included in COMMONS as per COMMONS-72. We can point to it when it is released.
