93c22650232b48fd3375c6c2b1e92f00eef2d615
Javier Quiroz <javier.quiroz@forgerock.com>
2016-03-18 09:33:36 -0700

    OPENIDM-3667 -- openidm/managed/user/openidm-admin 404 (Not Found) when selecting process instance created by openidm-admin

--- openidm-ui/openidm-ui-admin/src/test/nightwatchjs/pages/configureManagedObjectsList.js
+++ openidm-ui/openidm-ui-admin/src/test/nightwatchjs/pages/configureManagedObjectsList.js
@@ -2,6 +2,20 @@ module.exports = {
     url: function() {
         return this.api.globals.baseUrl + "#managed/";
     },
+    elements: {
+        cards: "#managedCardContainer > div.row > div",
+        managedCardContainer: "#managedCardContainer",
+        managedCardToggle: 'a[href="#managedCardContainer"]',
+        cardDropdownButton: ".card button.dropdown-toggle",
+        cardDropdownList: ".card ul.dropdown-menu",
+        cardDeleteButton: ".card ul.dropdown-menu li.managed-delete",
+        gridRows: '#managedGrid > table > tbody > tr',
+        managedGridContainer: "#managedGridContainer",
+        managedGridToggle: 'a[href="#managedGridContainer"]',
+        gridDropdownButton: "#managedGridContainer table > tbody > tr:first-child button.dropdown-toggle",
+        gridDropdownList: "#managedGridContainer ul.dropdown-menu",
+        gridDeleteButton: "#managedGridContainer ul.dropdown-menu li.managed-delete"
+    },
     sections: {
         toolbar: {
             selector: ".toolbar",
