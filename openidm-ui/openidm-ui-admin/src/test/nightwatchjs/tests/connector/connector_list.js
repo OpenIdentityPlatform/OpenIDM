e6ac7f83bdfe3baf13e74e15e363445a2252f583
jason.browne <jason.browne@forgerock.com>
2016-03-21 13:50:57 -0700

    OPENIDM-5210 - Expand connector list nightwatch tests

--- openidm-ui/openidm-ui-admin/src/test/nightwatchjs/tests/connector/connector_list.js
+++ openidm-ui/openidm-ui-admin/src/test/nightwatchjs/tests/connector/connector_list.js
@@ -30,7 +30,7 @@ module.exports = {
         );
     },
     after: function(client) {
-        client.end();
+       client.end();
     },
     'Connector List Page Loaded': function (client) {
         var connectors = client.page.connectors(),
@@ -44,6 +44,64 @@ module.exports = {
         connectorList
             .waitForElementVisible('@addCard', 2000);
     },
+    'Connector List Display Toggle' : function(client) {
+        var connectors = client.page.connectors(),
+            connectorList = connectors.section.connectorList;
+
+        connectorList
+            .click('@toggleGridViewButton')
+            .waitForElementVisible('@gridView', 1000)
+            .assert.visible('@gridView', "Successfully displaying grid")
+            .click('@toggleCardViewButton')
+            .assert.visible('@addCard', "Successfully displaying cards")
+            .waitForElementVisible('@addCard', 1000);
+    },
+    'Connector List Filter' : function(client) {
+        var connectors = client.page.connectors(),
+            connectorList = connectors.section.connectorList;
+
+        connectorList
+            .click('@connectorListFilter')
+            .setValue('@connectorListFilter', 'noo');
+
+        client
+            .pause(2000);
+
+        connectorList
+            .clearValue('@connectorListFilter')
+            .assert.hidden('@cards', "Card filtered out of display")
+            .click('@connectorListFilter')
+            .setValue('@connectorListFilter', 'tes');
+
+        client
+            .pause(2000);
+
+        connectorList
+            .assert.visible('@cards', "Card no longer filtered out of display");
+    },
+    'Connector grid view drop down' : function(client) {
+        var connectors = client.page.connectors(),
+            connectorList = connectors.section.connectorList;
+
+        connectorList
+            .click('@toggleGridViewButton')
+            .waitForElementVisible('@gridView', 1000)
+            .click('@connectorGridToggle')
+            .waitForElementVisible('@connectorGridToggle', 1000)
+            .click('@toggleCardViewButton')
+            .waitForElementVisible('@addCard', 1000);
+    },
+    'New Connector Page Change' : function(client) {
+        var connectors = client.page.connectors(),
+            connectorList = connectors.section.connectorList;
+
+        connectorList
+            .click('@addCard')
+            .waitForElementVisible('@backToConnector', 1000)
+            .click('@backToConnector')
+            .waitForElementVisible('@addCard', 1000);
+
+     },
     'Connector Successfully Deleted' : function(client) {
         var connectors = client.page.connectors(),
             connectorList = connectors.section.connectorList,
@@ -52,7 +110,7 @@ module.exports = {
 
         connectorList
             .waitForElementVisible('@connectorCard', 2000)
-            .click('@connectorToggle')
+            .click('@connectorListToggle')
             .waitForElementVisible('@connectorDropdown', 1000)
             .click('@connectorDelete');
 
@@ -64,6 +122,5 @@ module.exports = {
 
         message
             .expect.element('@displayMessage').text.to.equal("Connector successfully deleted.");
-
     }
 };
