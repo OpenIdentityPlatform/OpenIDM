c6961a8ca09a6265b196fea7bac039fa73d34f65
Jim Mitchener <jim.mitchener@forgerock.com>
2016-04-28 15:39:40 -0800

    OPENIDM-5272 fix Oracle updateobject id incrementing trigger

--- openidm-zip/src/main/resources/db/oracle/scripts/openidm.sql
+++ openidm-zip/src/main/resources/db/oracle/scripts/openidm.sql
@@ -1180,23 +1180,23 @@ END;
 
 /
 
-CREATE OR REPLACE TRIGGER clusterobjects_id_TRG BEFORE INSERT ON clusterobjects
+CREATE OR REPLACE TRIGGER updateobjects_id_TRG BEFORE INSERT ON updateobjects
 FOR EACH ROW
   DECLARE
     v_newVal NUMBER(12) := 0;
     v_incval NUMBER(12) := 0;
   BEGIN
     IF INSERTING AND :new.id IS NULL THEN
-      SELECT  clusterobjects_id_SEQ.NEXTVAL INTO v_newVal FROM DUAL;
+      SELECT  updateobjects_id_SEQ.NEXTVAL INTO v_newVal FROM DUAL;
 -- If this is the first time this table have been inserted into (sequence == 1)
       IF v_newVal = 1 THEN
 --get the max indentity value from the table
-        SELECT NVL(max(id),0) INTO v_newVal FROM clusterobjects;
+        SELECT NVL(max(id),0) INTO v_newVal FROM updateobjects;
         v_newVal := v_newVal + 1;
 --set the sequence to that value
         LOOP
           EXIT WHEN v_incval>=v_newVal;
-          SELECT clusterobjects_id_SEQ.nextval INTO v_incval FROM dual;
+          SELECT updateobjects_id_SEQ.nextval INTO v_incval FROM dual;
         END LOOP;
       END IF;
 --used to emulate LAST_INSERT_ID()
