7716dd06a3865ae17fef711b68e0e132b2c20bf1
Alin Brici <alin.brici@forgerock.com>
2016-05-17 10:09:50 -0700

    OPENIDM-5830: Fix terminiology for deleting a role exception

--- openidm-zip/src/main/resources/bin/defaults/script/roles/onDelete-roles.js
+++ openidm-zip/src/main/resources/bin/defaults/script/roles/onDelete-roles.js
@@ -1,25 +1,17 @@
-/**
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
  *
- * Copyright (c) 2014 ForgeRock AS. All rights reserved.
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
  *
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the License). You may not use this file except in
- * compliance with the License.
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
  *
- * You can obtain a copy of the License at
- * http://forgerock.org/license/CDDLv1.0.html
- * See the License for the specific language governing
- * permission and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL
- * Header Notice in each file and include the License file
- * at http://forgerock.org/license/CDDLv1.0.html
- * If applicable, add the following below the CDDL Header,
- * with the fields enclosed by brackets [] replaced by
- * your own identifying information:
- * "Portions Copyrighted [year] [name of copyright owner]"
+ * Copyright 2014-2016 ForgeRock AS.
  */
 
 /**
@@ -35,6 +27,6 @@ var users = openidm.query(resourcePath, {"_queryFilter": "true"}, ["*"]).result;
 if (users.length > 0) {
     throw {
         "code" : 409,
-        "message" : "Cannot delete a role that is currently assigned"
+        "message" : "Cannot delete a role that is currently granted"
     };
 }
