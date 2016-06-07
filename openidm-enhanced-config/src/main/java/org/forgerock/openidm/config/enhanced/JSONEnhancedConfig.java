dc809192c21a6c687b06b8413cc130f03a7c8f49
Brendan Miller <brendan.miller@forgerock.com>
2016-06-06 19:03:34 -0700

    openidm-enhanced-config warning cleanup

--- openidm-enhanced-config/src/main/java/org/forgerock/openidm/config/enhanced/JSONEnhancedConfig.java
+++ openidm-enhanced-config/src/main/java/org/forgerock/openidm/config/enhanced/JSONEnhancedConfig.java
@@ -30,6 +30,8 @@ import java.util.Map;
 
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.ConfigurationPolicy;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
 import org.forgerock.json.JsonException;
 import org.forgerock.json.JsonTransformer;
@@ -45,7 +47,6 @@ import org.forgerock.util.Reject;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.ComponentContext;
-import org.osgi.util.tracker.ServiceTracker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -57,8 +58,11 @@ import org.slf4j.LoggerFactory;
         policy = ConfigurationPolicy.IGNORE,
         description = "OpenIDM Enhanced Config Service",
         immediate = true,
-        metatype = false)
+        metatype = true)
 @Service
+@Properties(
+        @Property(name = "suppressMetatypeWarning", value = "true")
+)
 public class JSONEnhancedConfig implements EnhancedConfig {
 
     public static final String PID = "org.forgerock.openidm.config.enhanced";
@@ -74,28 +78,6 @@ public class JSONEnhancedConfig implements EnhancedConfig {
      */
     private final static Logger logger = LoggerFactory.getLogger(JSONEnhancedConfig.class);
 
-    // Disable the property escaping by default
-    private boolean doEscape = false;
-
-    // Keep track of the cryptography OSGi service
-    private static ServiceTracker cryptoTracker;
-
-    /**
-     * Sets the escaping mode.
-     * <p/>
-     * If {@code true} then this newBuilder processes the escapes {@code \}
-     * character in the sting values otherwise it does not handle specially the
-     * {@code \} character.
-     *
-     * @param escape
-     *            {@code true} to enable or {@code false} to disable.
-     * @return this newBuilder.
-     */
-    public JSONEnhancedConfig setEscaping(boolean escape) {
-        doEscape = escape;
-        return this;
-    }
-
     public String getConfigurationFactoryPid(ComponentContext compContext) {
         Object o = compContext.getProperties().get(ServerConstants.CONFIG_FACTORY_PID);
         if (o instanceof String) {
@@ -105,7 +87,7 @@ public class JSONEnhancedConfig implements EnhancedConfig {
     }
 
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
     public Map<String, Object> getConfiguration(ComponentContext compContext)
             throws InvalidException, InternalErrorException {
@@ -121,12 +103,10 @@ public class JSONEnhancedConfig implements EnhancedConfig {
             throws InvalidException, InternalErrorException {
         Reject.ifNull(compContext);
 
-        Dictionary dict = compContext.getProperties();
-        String servicePid = (String) compContext.getProperties().get(Constants.SERVICE_PID);
+        Dictionary<String, Object> dict = compContext.getProperties();
+        String servicePid = (String) dict.get(Constants.SERVICE_PID);
 
-        JsonValue conf = getConfiguration(dict, compContext.getBundleContext(), servicePid);
-
-        return conf;
+        return getConfiguration(dict, compContext.getBundleContext(), servicePid);
     }
 
     /**
@@ -138,8 +118,7 @@ public class JSONEnhancedConfig implements EnhancedConfig {
     }
 
     /**
-     * {@see getConfiguration(Dictionary<String, Object>, BundleContext, String)
-     * * }
+     * {@see getConfiguration(Dictionary, BundleContext, String)}
      *
      * @param decrypt
      *            true if any encrypted values should be decrypted in the result
@@ -173,6 +152,7 @@ public class JSONEnhancedConfig implements EnhancedConfig {
 
         JsonValue decrypted = jv;
         if (!jv.isNull()) {
+            boolean doEscape = false;
             decrypted.getTransformers().add(new PropertyTransformer(doEscape));
             decrypted.applyTransformers();
             decrypted = decrypted.copy();
@@ -193,7 +173,7 @@ public class JSONEnhancedConfig implements EnhancedConfig {
         return CryptoServiceFactory.getInstance();
     }
 
-    public static class PropertyTransformer implements JsonTransformer {
+    private static class PropertyTransformer implements JsonTransformer {
 
         // Disable the property escaping by default
         private final boolean doEscape;
@@ -210,157 +190,4 @@ public class JSONEnhancedConfig implements EnhancedConfig {
             }
         }
     }
-
-    // private <T> T getSubstituteValue(Class<T> type, String variable,
-    // final IdentityServer identityServer) {
-    // T substValue = null;
-    // if (String.class.isAssignableFrom(type)) {
-    // // Get the value of the deepest nested variable
-    // // placeholder.
-    // // Try to configuration properties first.
-    // substValue =
-    // (identityServer != null) ? (T) identityServer.getProperty(variable, null,
-    // String.class)
-    // : null;
-    // if (substValue == null) {
-    // // Ignore unknown property values.
-    // substValue = (T) System.getProperty(variable);
-    // }
-    // } else {
-    // substValue =
-    // (identityServer != null) ? identityServer.getProperty(variable, null,
-    // type)
-    // : null;
-    // if (substValue == null) {
-    // // Ignore unknown property values.
-    // substValue = (T) System.getProperty(variable);
-    // }
-    // }
-    // if (null == substValue){
-    // logger.warn("Undefined property '{}' used for substitution.", variable);
-    // }
-    // return substValue;
-    // }
-    //
-    // private static final String DELIM_START = "&{";
-    // private static final char DELIM_STOP = '}';
-    //
-    // /**
-    // * <p>
-    // * This method performs property variable substitution on the specified
-    // * value. If the specified value contains the syntax
-    // * <tt>&{&lt;prop-name&gt;}</tt>, where <tt>&lt;prop-name&gt;</tt> refers
-    // to
-    // * either a configuration property or a system property, then the
-    // * corresponding property value is substituted for the variable
-    // placeholder.
-    // * Multiple variable placeholders may exist in the specified value as well
-    // * as nested variable placeholders, which are substituted from inner most
-    // to
-    // * outer most. Configuration properties override system properties.
-    // * </p>
-    // *
-    // * @param val
-    // * The string on which to perform property substitution.
-    // * @param identityServer
-    // * Set of configuration properties.
-    // * @return The value of the specified string after property substitution.
-    // **/
-    // private Object substVars(String val, final IdentityServer identityServer)
-    // {
-    //
-    // // Assume we have a value that is something like:
-    // // "leading &{foo.&{bar}} middle ${baz} trailing"
-    //
-    // int stopDelim = -1;
-    // int startDelim = -1;
-    //
-    // if (!doEscape) {
-    // stopDelim = val.indexOf(DELIM_STOP, stopDelim + 1);
-    // // If there is no stopping delimiter, then just return
-    // // the value since there is no variable declared.
-    // if (stopDelim < 0) {
-    // return val;
-    // }
-    // startDelim = val.indexOf(DELIM_START);
-    // // If there is no starting delimiter, then just return
-    // // the value since there is no variable declared.
-    // if (startDelim < 0) {
-    // return val;
-    // }
-    // }
-    //
-    // StringBuilder parentBuilder = new StringBuilder(val.length());
-    // Stack<StringBuilder> propertyStack = new Stack<StringBuilder>();
-    // propertyStack.push(parentBuilder);
-    //
-    // for (int index = 0; index < val.length(); index++) {
-    // switch (val.charAt(index)) {
-    // case '\\': {
-    // if (doEscape) {
-    // index++;
-    // if (index < val.length()) {
-    // propertyStack.peek().append(val.charAt(index));
-    // }
-    // } else {
-    // propertyStack.peek().append(val.charAt(index));
-    // }
-    // break;
-    // }
-    // case '&': {
-    // if ('{' == val.charAt(index + 1)) {
-    // // This is a start of a new property
-    // propertyStack.push(new StringBuilder(val.length()));
-    // index++;
-    // } else {
-    // propertyStack.peek().append(val.charAt(index));
-    // }
-    // break;
-    // }
-    // case DELIM_STOP: {
-    // // End of the actual property
-    // if (propertyStack.size() == 1) {
-    // // There is no start delimiter
-    // propertyStack.peek().append(val.charAt(index));
-    // } else {
-    // String variable = propertyStack.pop().toString();
-    // if ((index == val.length() - 1) && propertyStack.size() == 1
-    // && parentBuilder.length() == 0) {
-    // // Replace entire value with an Object
-    // Object substValue =
-    // getSubstituteValue(Object.class, variable, identityServer);
-    // if (null != substValue) {
-    // return substValue;
-    // } else {
-    // propertyStack.peek().append(DELIM_START).append(variable).append(
-    // DELIM_STOP);
-    // return propertyStack.peek().toString();
-    // }
-    // } else {
-    // String substValue =
-    // getSubstituteValue(String.class, variable, identityServer);
-    // if (null != substValue) {
-    // propertyStack.peek().append(substValue);
-    // } else {
-    // propertyStack.peek().append(DELIM_START).append(variable).append(
-    // DELIM_STOP);
-    // }
-    // }
-    // }
-    // break;
-    // }
-    // default: {
-    // propertyStack.peek().append(val.charAt(index));
-    // }
-    // }
-    // }
-    //
-    // // Close the open &{ tags.
-    // for (int index = propertyStack.size(); index > 1; index--) {
-    // StringBuilder top = propertyStack.pop();
-    // propertyStack.peek().append(DELIM_START).append(top.toString());
-    // }
-    // return parentBuilder.toString();
-    // }
-
 }
