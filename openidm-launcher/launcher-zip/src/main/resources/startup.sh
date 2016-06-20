43045e10ef12f2b9786b031a080e39ef8b8aa87b
Brendan Miller <brendan.miller@forgerock.com>
2016-06-20 15:13:35 -0700

    Revert "Initial commit of the new ForgeRock Launcher project"
    This reverts commit f7bca40974f149bc465c215b5bba3c2ddc245498.

--- openidm-launcher/launcher-zip/src/main/resources/startup.sh
+++
@@ -1,5 +0,0 @@
-CLASSPATH="bin/*:framework/*"
-
-exec java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 \
-    -classpath "$CLASSPATH" \
-	org.forgerock.commons.launcher.Main
