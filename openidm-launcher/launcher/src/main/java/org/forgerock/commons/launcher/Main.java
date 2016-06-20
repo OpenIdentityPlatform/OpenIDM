5dcb0eae5c0fe66e4d2fd1180567d9592a676627
Brendan Miller <brendan.miller@forgerock.com>
2016-06-20 15:11:55 -0700

    Revert "Refactored to have two services exposed. Allow Restart"
    This reverts commit 7da358baf99b50eee7bb5af41b3922c6d5cce4fb.

--- openidm-launcher/launcher/src/main/java/org/forgerock/commons/launcher/Main.java
+++ openidm-launcher/launcher/src/main/java/org/forgerock/commons/launcher/Main.java
@@ -1,17 +1,25 @@
 /*
- * The contents of this file are subject to the terms of the Common Development and
- * Distribution License (the License). You may not use this file except in compliance with the
- * License.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
- * specific language governing permission and limitations under the License.
+ * Copyright (c) 2012 ForgeRock AS. All Rights Reserved
  *
- * When distributing Covered Software, include this CDDL Header Notice in each file and include
- * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
- * Header, with the fields enclosed by brackets [] replaced by your own identifying
- * information: "Portions copyright [year] [name of copyright owner]".
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
  *
- * Copyright 2012-2015 ForgeRock AS.
+ * You can obtain a copy of the License at
+ * http://forgerock.org/license/CDDLv1.0.html
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at http://forgerock.org/license/CDDLv1.0.html
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
  */
 
 package org.forgerock.commons.launcher;
@@ -40,10 +48,6 @@ public class Main {
             // The Framework was started by the Main thread and the Framework
             // was stopped.
             // Some Bundle may keep thread and prevent the System from stopping
-            if (server.restart.get()) {
-                // Send -1 to indicate a restart to the start script
-                System.exit(-1);
-            }
             System.exit(0);
         }
     }
