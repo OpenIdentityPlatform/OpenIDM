757abec87e2fe64bc386c3062a9a2cf3f8b1452c
Brendan Miller <brendan.miller@forgerock.com>
2016-06-20 15:12:00 -0700

    Revert "OPENIDM-482 - Update the service installer script"
    This reverts commit bc53227bb689e9ba4e7bcd5ee68a773314e0f80d.

--- openidm-launcher/launcher-zip/src/main/resources/bin/launcher.bat
+++ openidm-launcher/launcher-zip/src/main/resources/bin/launcher.bat
@@ -1,25 +1,24 @@
-rem @echo off
+@echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-rem Set Launcher Home
-set CURRENT_DIR=%cd%
-cd /d %0\..
-set SCRIPT_DIR=%cd%
+rem Guess FORGEROCK_HOME if not defined
+set "CURRENT_DIR=%cd%"
+if not "%FORGEROCK_HOME%" == "" goto homeSet
+set FORGEROCK_HOME=%CURRENT_DIR%
+if exist "%FORGEROCK_HOME%\bin\launcher.bat" goto homeOk
 cd ..
-if not "%LAUNCHER_SERVER_HOME%" == "" goto homeSet
-set LAUNCHER_SERVER_HOME=%cd%
+set FORGEROCK_HOME=%cd%
 :homeSet
-cd "%CURRENT_DIR%""
-if exist "%LAUNCHER_SERVER_HOME%\bin\launcher.bat" goto homeOk
-echo Invalid LAUNCHER_SERVER_HOME environment variable
-echo Please set it to correct Launcher Home
-goto exit
+
+if exist "%FORGEROCK_HOME%\bin\launcher.bat" goto homeOk
+echo Invalid FORGEROCK_HOME environment variable
+echo Please set it to correct ForgeRock Launcher Home
 :homeOk
 
 rem Check Java availability
 if not "%JAVA_HOME%" == "" goto checkJavaHome
 if not "%JRE_HOME%" == "" goto checkJavaHome
-echo JAVA_HOME or JRE_HOME not available, Java is needed to run the Connector Server
+echo JAVA_HOME or JRE_HOME not available, Java is needed to run the ForgeRock Launcher
 echo Please install Java and set the JAVA_HOME accordingly
 goto exit
 :checkJavaHome
@@ -36,15 +35,14 @@ set JAVA="%JAVA_HOME%\bin\java.exe"
 set JAVA_DLL="%JAVA_HOME%\jre\bin\server\jvm.dll"
 :homeOk
 
-rem Check and Set CLASSPATH for starting Launcher
-if not "%CP%" == "" goto classpathOK
-set CP="%SCRIPT_DIR%\*"
-:classpathOK
+rem Set CLASSPATH for starting ForgeRock Launcher
+set CP="bin/*;framework/*"
+
 
 rem SET MISC PROPERTIES
 rem Architecture, can be i386 or amd64 or ia64 (it is basically the directory name
-rem where the binaries are stored, if not set this script will try to 
-rem find the value automatically based on environment variables)
+rem   where the binaries are stored, if not set this script will try to 
+rem   find the value automatically based on environment variables)
 set ARCH=
 rem find out the architecture
 if ""%ARCH%"" == """" (
@@ -53,42 +51,75 @@ if ""%ARCH%"" == """" (
   if ""%PROCESSOR_ARCHITECTURE%"" == ""IA64""  set ARCH=ia64
 )
 
+rem Run java options, needs to be separated by space
+set JAVA_OPTS=-Xmx1024m
 
-rem -------------------------------------------------------------
-rem Service java options, needs to be separated by ;
-if not "%JAVA_OPTS_SERVICE%" == "" goto optsOK
-set JAVA_OPTS_SERVICE=-Xmx128m;-Xms128m; 
-:optsOK
-
-rem Set Launcher start params, needs to be separated by ;
-if not "%LAUNCHER_START_PARAMS%" == "" goto launcerStartOK
-set LAUNCHER_START_PARAMS=-c;bin/launcher.json
-:launcerStartOK
-rem -------------------------------------------------------------
+rem Service java options, needs to be separated by ; or #
+set JAVA_OPTS_SERVICE=-Xmx1024m
+set MAIN_CLASS=org.forgerock.commons.launcher.Main
+set JVM_OPTION_IDENTIFIER=-J
 
 
+if ""%1"" == ""/run"" goto srvRun
 if ""%1"" == ""/install"" goto srvInstall
 if ""%1"" == ""/uninstall"" goto srvUninstall
 
-echo Usage: launcher.bat ^<command^> ^[option^]
+echo Usage: launcher ^<command^> ^[option^]
 echo command:
-echo    /install ^[^<serviceName^>^] - Installs the service.
+echo    /install ^[^<serviceName^>^] ^["-J<java option>"^] - Installs the service.
 echo    /uninstall ^[^<serviceName^>^] - Uninstalls the service.
+echo    /run ^["-J<java option>"^] - Runs the server from the console.
 echo.
+echo example:
+echo     launcher.bat /install
+echo        - this will install launcher as service
+echo.    
+echo     launcher.bat /run "-J-Xdebug" "-J-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
+echo        - this will run launcher in debug mode
+goto :EOF
+
+:srvRun
+rem Run the server main class
+shift
+set JAVA_OPTS_PARAM=
+set JAVA_OPTS_DELIM=
+for %%P in (%*) do (
+    set T=%%P
+    if "!T:~1,2!" == "%JVM_OPTION_IDENTIFIER%" (
+      set JAVA_OPTS_PARAM=!JAVA_OPTS_PARAM!!JAVA_OPTS_DELIM!!T:~3,-1!
+      set JAVA_OPTS_DELIM= 
+    )
+)
+cd "%FORGEROCK_HOME%"
+
+%JAVA% %JAVA_OPTS% %JAVA_OPTS_PARAM% -server -classpath %CP% %MAIN_CLASS%
+cd "%CURRENT_DIR%"
 goto :EOF
 
 
 :srvInstall
-rem Install the ForgeRock Launcher as Windows service
+rem Install the Connector Server as Windows service
 shift
 set SERVICE_NAME=ForgeRockLauncherJavaService
 if not ""%1"" == """" (
+    set T=%1
+    if "!T:~1,2!" == "%JVM_OPTION_IDENTIFIER%" goto :noServiceName
     set SERVICE_NAME=%1
 )
 shift
 :noServiceName
-set MAIN_CLASS=org.forgerock.commons.launcher.Main
-"%LAUNCHER_SERVER_HOME%\bin\%ARCH%\prunsrv.exe" //IS//%SERVICE_NAME% --Install="%LAUNCHER_SERVER_HOME%\bin\%ARCH%\prunsrv.exe" --Description="ForgeRock OSGi Java Server" --Jvm=%JAVA_DLL% --Classpath=%CP% --JvmOptions=%JAVA_OPTS_SERVICE%%JAVA_OPTS_PARAM% --StartPath="%LAUNCHER_SERVER_HOME%" --StartMode=jvm --StartClass=%MAIN_CLASS% --StartMethod=start --StartParams="%SERVER_START_PARAMS%" --StopMode=jvm --StopClass=%MAIN_CLASS% --StopMethod=stop --LogPath="%LAUNCHER_SERVER_HOME%\logs" --LogPrefix=launcher --StdOutput=auto --StdError=auto --LogLevel=INFO
+set JAVA_OPTS_PARAM=
+set JAVA_OPTS_DELIM=
+for %%P in (%*) do (
+    set T=%%P
+    if "!T:~1,2!" == "%JVM_OPTION_IDENTIFIER%" (
+      set JAVA_OPTS_PARAM=!JAVA_OPTS_PARAM!!JAVA_OPTS_DELIM!!T:~3,-1!
+      set JAVA_OPTS_DELIM=;
+    )
+)
+"%FORGEROCK_HOME%\bin\%ARCH%\prunsrv.exe" //IS//%SERVICE_NAME% --Install="%FORGEROCK_HOME%\bin\%ARCH%\prunsrv.exe" --Description="ForgeRock OSGi Java Server" --Jvm=%JAVA_DLL% --JvmOptions=%JAVA_OPTS_SERVICE%%JAVA_OPTS_PARAM% --Classpath=%CP% --StartMode=jvm --StartPath="%FORGEROCK_HOME%" --StartClass=%MAIN_CLASS% --StartMethod=start --StartParams=dummy --StopMode=jvm --StopClass=%MAIN_CLASS% --StopMethod=stop --StopParams=dummy --LogPath="%FORGEROCK_HOME%\logs" --LogPrefix=service --LogLevel=INFO --StdOutput=auto --StdError=auto
+
+
 echo ForgeRock Launcher Java Service successfully installed as "%SERVICE_NAME%" service
 goto :EOF
 
@@ -99,8 +130,8 @@ if not ""%1"" == """" (
 ) else (
     set SERVICE_NAME=ForgeRockLauncherJavaService
 )
-"%LAUNCHER_SERVER_HOME%\bin\%ARCH%\prunsrv.exe" //DS//%SERVICE_NAME%
-echo Service "%SERVICE_NAME%" removed successfully
+"%FORGEROCK_HOME%\bin\%ARCH%\prunsrv.exe" //DS//%SERVICE_NAME%
+echo ForgeRock Launcher Java Service "%SERVICE_NAME%" removed successfully
 goto :EOF
 
 :exit
