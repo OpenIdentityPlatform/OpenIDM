893986d24bbc28f520e5c2b2f9e2900d5e8f60b4
Jim Mitchener <jim.mitchener@forgerock.com>
2016-07-08 12:29:03 -0800

    OPENIDM-6186 Remove conf backup from update

--- openidm-maintenance/src/main/java/org/forgerock/openidm/maintenance/upgrade/UpdateManagerImpl.java
+++ openidm-maintenance/src/main/java/org/forgerock/openidm/maintenance/upgrade/UpdateManagerImpl.java
@@ -1208,15 +1208,7 @@ public class UpdateManagerImpl implements UpdateManager {
          */
         private void patchConfig(final Context context, final Path configFile, final JsonValue patch) throws UpdateException {
             final String pid = parsePid(configFile.getFileName().toString());
-            final Path backupFile = installDir.resolve("conf/").resolve(configFile.getFileName().toString()
-                    + StaticFileUpdate.OLD_SUFFIX + timestamp);
 
-            try {
-                final Path projectDir = Paths.get(IdentityServer.getInstance().getProjectLocation().toString());
-                Files.copy(projectDir.resolve("conf/").resolve(configFile.getFileName()), backupFile);
-            } catch (IOException e) {
-                logger.warn("Config backup failed for " + configFile, e);
-            }
             try {
                 PatchRequest request = Requests.newPatchRequest("config/" + pid);
                 for (PatchOperation op : PatchOperation.valueOfList(patch)) {
