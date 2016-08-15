fa4579b1a63d92814c8b0c8a8c2b137658324380
Joy Feng <joy.feng@forgerock.com>
2016-08-15 15:56:48 -0700

    OPENIDM-6370 Create backend endpoint to allow endusers to unbind from a social provider
    	new file:   openidm-zip/src/main/resources/bin/defaults/script/ui/unBindBehavior.js
    	modified:   openidm-zip/src/main/resources/script/access.js
    	modified:   openidm-zip/src/main/resources/bin/defaults/script/ui/unBindBehavior.js
    	modified:   openidm-zip/src/main/resources/script/access.js

--- openidm-zip/src/main/resources/script/access.js
+++ openidm-zip/src/main/resources/script/access.js
@@ -1,28 +1,18 @@
-/**
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
  *
- * Copyright (c) 2011-2015 ForgeRock AS. All rights reserved.
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
  *
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the License). You may not use this file except in
- * compliance with the License.
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
  *
- * You can obtain a copy of the License at
- * http://forgerock.org/license/CDDLv1.0.html
- * See the License for the specific language governing
- * permission and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL
- * Header Notice in each file and include the License file
- * at http://forgerock.org/license/CDDLv1.0.html
- * If applicable, add the following below the CDDL Header,
- * with the fields enclosed by brackets [] replaced by
- * your own identifying information:
- * "Portions Copyrighted [year] [name of copyright owner]"
+ * Copyright 2011-2016 ForgeRock AS.
  */
-
-
 // A configuration for allowed HTTP requests. Each entry in the configuration contains a pattern
 // to match against the incoming request ID and, in the event of a match, the associated roles,
 // methods, and actions that are allowed for requests on that particular pattern.
@@ -257,8 +247,8 @@ var httpAccessConfig =
         {
             "pattern"   : "*",
             "roles"     : "openidm-authorized",
-            "methods"   : "read",
-            "actions"   : "*",
+            "methods"   : "read,action",
+            "actions"   : "unbind",
             "customAuthz" : "ownDataOnly()"
         },
         {
