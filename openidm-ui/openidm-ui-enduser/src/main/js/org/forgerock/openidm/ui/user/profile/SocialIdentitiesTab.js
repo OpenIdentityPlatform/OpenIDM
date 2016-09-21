e7dd2894b3420f811a935c5e24bbf56575f95a51
oliver.bradley <oliver.bradley@forgerock.com>
2016-09-21 09:11:13 -0700

    OPENIDM-6637 - end user can no longer unbind last provider with no password set
    fixes

--- openidm-ui/openidm-ui-enduser/src/main/js/org/forgerock/openidm/ui/user/profile/SocialIdentitiesTab.js
+++ openidm-ui/openidm-ui-enduser/src/main/js/org/forgerock/openidm/ui/user/profile/SocialIdentitiesTab.js
@@ -26,28 +26,31 @@ define([
     "org/forgerock/commons/ui/common/util/OAuth",
     "org/forgerock/commons/ui/common/main/Router",
     "org/forgerock/openidm/ui/common/delegates/SocialDelegate",
-    "org/forgerock/openidm/ui/common/util/OAuthUtils"
+    "org/forgerock/commons/ui/common/util/UIUtils",
+    "org/forgerock/openidm/ui/common/UserModel"
 ], function($, _, bootstrap,
-            BootstrapDialog,
-            AbstractUserProfileTab,
-            Configuration,
-            Constants,
-            EventManager,
-            OAuth,
-            Router,
-            SocialDelegate,
-            OAuthUtils
+    BootstrapDialog,
+    AbstractUserProfileTab,
+    Configuration,
+    Constants,
+    EventManager,
+    OAuth,
+    Router,
+    SocialDelegate,
+    UIUtils,
+    UserModel
 ) {
     var SocialIdentitiesView = AbstractUserProfileTab.extend({
         template: "templates/profile/SocialIdentitiesTab.html",
         events: _.extend({
-            "click .social-toggle" : "toggleAction"
+            "click .social-toggle": "toggleAction",
+            "click .closeErrorMsg": "closeErrorMsg"
         }, AbstractUserProfileTab.prototype.events),
 
         /**
          Expected by all dynamic user profile tabs - returns a map of details necessary to render the nav tab
          */
-        getTabDetail : function () {
+        getTabDetail: function() {
             return {
                 "panelId": "socialIdentities",
                 "label": $.t("templates.socialIdentities.socialIdentities")
@@ -57,32 +60,48 @@ define([
         model: {},
 
         render: function(args, callback) {
-            this.data.user = Configuration.loggedUser.toJSON();
+            let params = Router.convertCurrentUrlToJSON().params;
 
-            SocialDelegate.providerList().then((response) => {
-                this.data.providers = response.providers;
+            if (!_.isEmpty(params) && _.has(params, "provider") && _.has(params, "code") && _.has(params, "redirect_uri")) {
 
-                _.each(this.data.providers, (provider, index) => {
-                    switch (provider.name) {
-                        case "google":
-                            provider.faIcon = "google";
-                            break;
-                        case "facebook":
-                            provider.faIcon = "facebook";
-                            break;
-                        case "linkedIn":
-                            provider.faIcon = "linkedin";
-                            break;
-                        default:
-                            provider.faIcon = "cloud";
-                            break;
-                    }
+                opener.require("org/forgerock/openidm/ui/user/profile/SocialIdentitiesTab").oauthReturn(params);
+                window.close();
+                return;
 
-                    this.activateProviders(provider, index);
+            } else {
+
+                this.data.user = Configuration.loggedUser.toJSON();
+                SocialDelegate.providerList().then((response) => {
+                    this.data.providers = response.providers;
+
+                    _.each(this.data.providers, (provider, index) => {
+                        switch (provider.name) {
+                            case "google":
+                                provider.faIcon = "google";
+                                break;
+                            case "facebook":
+                                provider.faIcon = "facebook";
+                                break;
+                            case "linkedIn":
+                                provider.faIcon = "linkedin";
+                                break;
+                            default:
+                                provider.faIcon = "cloud";
+                                break;
+                        }
+
+                        this.activateProviders(provider, index);
+                    });
+
+                    this.parentRender(() => {
+                        this.$el.find("#idpUnbindError").hide();
+                        if (callback) {
+                            callback();
+                        }
+                    });
                 });
 
-                this.parentRender(callback);
-            });
+            }
         },
 
         activateProviders: function(provider, index) {
@@ -93,19 +112,18 @@ define([
             }
         },
 
-        toggleSocialProvider: function(toggle) {
-
-            if(toggle.find("[type=checkbox]").prop("checked")) {
-                toggle.toggleClass("disabled", false);
-                toggle.find(".scopes").show();
+        toggleSocialProvider: function(card) {
+            if (card.find("[type=checkbox]").prop("checked")) {
+                card.toggleClass("disabled", false);
+                card.find(".scopes").show();
             } else {
-                toggle.toggleClass("disabled", true);
-                toggle.find(".scopes").hide();
+                card.toggleClass("disabled", true);
+                card.find(".scopes").hide();
             }
         },
 
-        getProviderName(toggle) {
-            return $(toggle).find(".card-body").data("name");
+        getProviderName(card) {
+            return $(card).find(".card-body").data("name");
         },
 
         getProviderObj(providerName) {
@@ -117,84 +135,130 @@ define([
         toggleAction: function(event) {
             event.preventDefault();
 
-            let toggle = $(event.target).parents(".card");
+            let card = $(event.target).parents(".card");
             let options = null;
 
-            if (!toggle.find("[type=checkbox]").prop("checked")) {
+            if (!card.find("[type=checkbox]").prop("checked")) {
 
-                options = this.getOptions(toggle, true);
-                OAuthUtils.oauthPopup(options);
-                this.toggleSocialProvider(toggle);
+                options = this.getOptions(card);
+                this.oauthPopup(options);
+                this.toggleSocialProvider(card);
 
             } else {
-                this.disconnectDialog(toggle);
+                this.disconnectDialog(card);
             }
         },
 
-        oauthReturn: function (params) {
-            Configuration.loggedUser.bindProvider(params.provider, params.code, params.redirect_uri).then(() => {
-                let toggle = this.$el.find(`.card-body[data-name="${params.provider}"]`).parents(".card");
+        oauthReturn: function(params) {
+            let card = this.$el.find(`.card-body[data-name="${params.provider}"]`).parents(".card");
 
-                toggle.find("[type=checkbox]").prop("checked", true);
+            Configuration.loggedUser.bindProvider(params.provider, params.code, params.redirect_uri).then(() => {
+                card.find("[type=checkbox]").prop("checked", true);
                 EventManager.sendEvent(Constants.EVENT_DISPLAY_MESSAGE_REQUEST, "saveSocialProvider");
-                this.toggleSocialProvider(toggle);
+                this.toggleSocialProvider(card);
             });
         },
 
-        getOptions: function(toggle) {
+        getOptions: function(card) {
             let options = {};
-            let providerName = this.getProviderName(toggle);
-            let state = "openerHandler/bind/&provider=" + providerName + "&redirect_uri=" + OAuth.getRedirectURI();
-            let url;
 
-            options.windowName = providerName;
-            url = OAuthUtils.getUrl(this.getProviderObj(options.windowName), state);
-            options.path = url + "&display=popup";
+            options.windowName = this.getProviderName(card);
+            options.path = this.getUrl(this.getProviderObj(options.windowName));
 
             return options;
         },
 
-        restoreToggle: function(toggle) {
-            $(toggle).prop("checked", false);
-            this.toggleSocialProvider(toggle);
+        oauthPopup: function(options) {
+            let oauthWindow = null;
+            let width = "";
+            let height = "";
+
+            width = screen.width * (2 / 3);
+            height = screen.height * (2 / 3);
+
+            options.windowName = options.windowName || 'ConnectWithOAuth';
+            options.windowOptions = options.windowOptions || 'location=0,status=0,width=' + width + ',height=' + height;
+            options.callback = options.callback || function() {
+                window.location.reload();
+            };
+
+            oauthWindow = window.open(options.path, options.windowName, options.windowOptions);
         },
 
-        disconnectDialog: function(toggle) {
+        disconnectDialog: function(card) {
 
             BootstrapDialog.show({
-                title: $.t("templates.socialIdentities.confirmTitle") + _.capitalize(this.getProviderName(toggle)) + "?",
+                title: $.t("templates.socialIdentities.confirmTitle") + _.capitalize(this.getProviderName(card)) + "?",
                 type: BootstrapDialog.TYPE_DANGER,
-                message: $.t("templates.socialIdentities.confirmMessage") + _.capitalize(this.getProviderName(toggle)) + ".",
+                message: $.t("templates.socialIdentities.confirmMessage") + _.capitalize(this.getProviderName(card)) + ".",
                 buttons: [{
                     label: $.t("common.form.cancel"),
-                    id:"disconnectCancel",
+                    id: "disconnectCancel",
                     action: function(dialogRef) {
                         dialogRef.close();
 
-                        $(toggle).prop('checked', true);
+                        $(card).prop('checked', true);
                     }
                 }, {
                     label: $.t('common.form.confirm'),
-                    id:"disconnectConfirm",
+                    id: "disconnectConfirm",
                     cssClass: "btn-danger",
                     action: _.bind(function(dialogRef) {
                         dialogRef.close();
 
-                        this.unbindProvider(toggle);
+                        this.unbindProvider(card);
                     }, this)
                 }]
             });
+
         },
 
-        unbindProvider: function(toggle) {
-            let providerName = this.getProviderName(toggle);
+        unbindProvider: function(card) {
+            let providerName = this.getProviderName(card);
 
-            Configuration.loggedUser.unbindProvider(providerName).then(() => {
-                toggle.find("[type=checkbox]").prop("checked", false);
+            Configuration.loggedUser.unbindProvider(providerName).then((result) => {
+                this.data.user = result;
+                card.find("[type=checkbox]").prop("checked", false);
                 EventManager.sendEvent(Constants.EVENT_DISPLAY_MESSAGE_REQUEST, "removeSocialProvider");
-                this.toggleSocialProvider(toggle);
+                this.toggleSocialProvider(card);
+            }, (err) => {
+                this.alignProvidersArray();
+                this.data.unbindText = $.t(err.responseJSON.message, { provider: _.startCase(providerName) });
+                UIUtils.renderTemplate(this.template, this.$el, this.data, $.noop(), "replace");
+                this.$el.find("#idpUnbindError").show();
+                delete this.data.unbindText;
             });
+
+        },
+
+        alignProvidersArray: function() {
+            _.each(this.data.providers, (provider, index) => {
+                provider.enabled = this.data.user.idpData[provider.name].enabled;
+            });
+        },
+
+        getUrl: function(provider) {
+            let scopes = provider.scope.join(" ");
+            let currentURL = Router.currentRoute;
+            let state = Router.getLink(currentURL, [
+                "&provider=" + provider.name +
+                "&redirect_uri=" + OAuth.getRedirectURI()
+            ]);
+
+            return OAuth.getRequestURL(provider.authorization_endpoint, provider.client_id, scopes, state);
+        },
+
+        getCurrentRoute() {
+            return Router.currentRoute;
+        },
+
+        closeErrorMsg: function(event) {
+            if (event) {
+                event.preventDefault();
+            }
+            this.$el.find("#idpUnbindError").hide();
         }
+
     });
 
     return new SocialIdentitiesView();
--- openidm-zip/src/main/resources/bin/defaults/script/ui/unBindBehavior.js
+++ openidm-zip/src/main/resources/bin/defaults/script/ui/unBindBehavior.js
@@ -14,16 +14,40 @@
  * Copyright 2016 ForgeRock AS.
  */
 
-if (object.idpData != null
-    && object.idpData[request.additionalParameters.provider] != null
-    && object.idpData[request.additionalParameters.provider].enabled === true) {
+if (object.idpData !== null &&
+    object.idpData !== undefined &&
+    object.idpData[request.additionalParameters.provider] !== null &&
+    object.idpData[request.additionalParameters.provider].enabled === true) {
 
-    // disable social provider via use of "enabled": false -
-    object.idpData[request.additionalParameters.provider].enabled = false;
+    var _ = require('lib/lodash'),
+        user = openidm.read(resourcePath),
+        enabledCount = 0,
+        disableProvider = function() {
+            // disable social provider via use of "enabled": false -
+            object.idpData[request.additionalParameters.provider].enabled = false;
 
-    // uncomment below line to delete social provider data -
-    // delete object.idpData[request.additionalParameters.provider];
+            // uncomment below line to delete social provider data -
+            // delete object.idpData[request.additionalParameters.provider];
 
-    // update the object in every case:
-    openidm.update(resourcePath, object._rev, object);
+            // update the object in every case:
+            openidm.update(resourcePath, object._rev, object);
+        };
+
+    if (!user.password) {
+        // make sure user isn't turning off last IDP with no password set
+        enabledCount = _.filter(_.values(object.idpData), function(idp) {
+            return idp.enabled;
+        }).length;
+
+        if (enabledCount > 1) {
+            disableProvider();
+        } else {
+            throw {
+                "code" : 400,
+                "message" : "config.messages.socialProviders.cannotUnbind"
+            };
+        }
+    } else {
+        disableProvider();
+    }
 }
