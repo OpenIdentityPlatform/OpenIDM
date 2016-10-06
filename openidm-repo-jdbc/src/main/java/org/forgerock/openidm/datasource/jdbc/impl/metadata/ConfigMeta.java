b4384cc137c12f4d01d89e3c76cc5805e0c13e63
Jason Vincent <jason.vincent@forgerock.com>
2016-10-06 11:46:32 -0700

    OPENIDM-2852: Encrypt the JDBC password - for clusters it requires keystore to not be in repo.

---
+++ openidm-repo-jdbc/src/main/java/org/forgerock/openidm/datasource/jdbc/impl/metadata/ConfigMeta.java
@@ -0,0 +1,56 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2011-2016 ForgeRock AS.
+ */
+package org.forgerock.openidm.datasource.jdbc.impl.metadata;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.forgerock.json.JsonPointer;
+import org.forgerock.json.JsonValue;
+import org.forgerock.openidm.datasource.jdbc.impl.JDBCDataSourceService;
+import org.forgerock.openidm.metadata.MetaDataProvider;
+import org.forgerock.openidm.metadata.MetaDataProviderCallback;
+
+/**
+ * Meta data provider to describe configuration requirements of this bundle
+ */
+public class ConfigMeta implements MetaDataProvider {
+
+    private static final JsonPointer PASSWORD_PTR = new JsonPointer("/password");
+    private final Map<String, List<JsonPointer>> propertiesToEncrypt;
+
+    public ConfigMeta() {
+        propertiesToEncrypt = new HashMap<>();
+        List<JsonPointer> props = new ArrayList<>();
+        props.add(PASSWORD_PTR);
+        propertiesToEncrypt.put(JDBCDataSourceService.PID, props);
+    }
+
+    @Override
+    public List<JsonPointer> getPropertiesToEncrypt(String pidOrFactory, String instanceAlias,
+            JsonValue config) {
+        if (propertiesToEncrypt.containsKey(pidOrFactory)) {
+            return propertiesToEncrypt.get(pidOrFactory);
+        }
+        return null;
+    }
+
+    @Override
+    public void setCallback(MetaDataProviderCallback callback) {
+    }
+}
