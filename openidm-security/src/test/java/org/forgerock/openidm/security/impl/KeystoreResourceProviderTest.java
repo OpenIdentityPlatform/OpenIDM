cf1f64f94fac85a4697004f78f4bf111b5eb50c0
Jason Lemay <jason.lemay@forgerock.com>
2016-10-25 14:19:16 -0700

    OPENIDM-6893 Remove the ability to retrieve private keys using the security service

--- openidm-security/src/test/java/org/forgerock/openidm/security/impl/KeystoreResourceProviderTest.java
+++ openidm-security/src/test/java/org/forgerock/openidm/security/impl/KeystoreResourceProviderTest.java
@@ -16,23 +16,21 @@
 
 package org.forgerock.openidm.security.impl;
 
+import static org.forgerock.json.JsonValue.field;
+import static org.forgerock.json.JsonValue.json;
+import static org.forgerock.json.JsonValue.object;
 import static org.forgerock.json.resource.test.assertj.AssertJActionResponseAssert.assertThat;
-import static org.forgerock.openidm.core.IdentityServer.*;
 import static org.forgerock.openidm.core.ServerConstants.LAUNCHER_INSTALL_LOCATION;
 import static org.forgerock.openidm.core.ServerConstants.LAUNCHER_PROJECT_LOCATION;
+import static org.forgerock.openidm.security.impl.SecurityTestUtils.createKeyStores;
 import static org.mockito.Mockito.mock;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
 import java.nio.file.Paths;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.Security;
 import java.security.cert.Certificate;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
 import org.assertj.core.api.Assertions;
@@ -40,18 +38,8 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.forgerock.json.JsonValue;
 import org.forgerock.json.resource.ActionRequest;
 import org.forgerock.json.resource.ActionResponse;
-import org.forgerock.json.resource.CreateRequest;
-import org.forgerock.json.resource.DeleteRequest;
-import org.forgerock.json.resource.InternalServerErrorException;
-import org.forgerock.json.resource.MemoryBackend;
-import org.forgerock.json.resource.QueryRequest;
-import org.forgerock.json.resource.QueryResourceHandler;
-import org.forgerock.json.resource.QueryResponse;
-import org.forgerock.json.resource.ReadRequest;
 import org.forgerock.json.resource.Requests;
 import org.forgerock.json.resource.ResourceException;
-import org.forgerock.json.resource.ResourceResponse;
-import org.forgerock.json.resource.UpdateRequest;
 import org.forgerock.json.resource.test.assertj.AssertJActionResponseAssert;
 import org.forgerock.json.test.assertj.AssertJJsonValueAssert;
 import org.forgerock.openidm.core.IdentityServer;
@@ -59,7 +47,6 @@ import org.forgerock.openidm.crypto.impl.CryptoServiceImpl;
 import org.forgerock.openidm.keystore.KeyStoreManagementService;
 import org.forgerock.openidm.keystore.KeyStoreService;
 import org.forgerock.openidm.keystore.impl.KeyStoreServiceImpl;
-import org.forgerock.openidm.repo.RepositoryService;
 import org.forgerock.openidm.util.DateUtil;
 import org.forgerock.services.context.RootContext;
 import org.forgerock.util.encode.Base64;
@@ -72,8 +59,8 @@ public class KeystoreResourceProviderTest {
     private static final String BEGIN_CERT = "-----BEGIN CERTIFICATE-----";
     private static final String END_CERT = "-----END CERTIFICATE-----";
 
-    private final String KEYSTORE_PASSWORD = "changeit";
-    private final String TEST_CERT_ALIAS = "testCert";
+    private static final String KEYSTORE_PASSWORD = "changeit";
+    private static final String TEST_CERT_ALIAS = "testCert";
 
     @BeforeClass
     public void runInitalSetup() throws Exception {
@@ -90,12 +77,12 @@ public class KeystoreResourceProviderTest {
     }
 
     @Test
-    public void testActionGenerateCertReturningPrivateKey() throws Exception {
+    public void testActionGenerateCert() throws Exception {
         //given
         createKeyStores();
         final KeyStoreServiceImpl keyStoreService = new KeyStoreServiceImpl();
         keyStoreService.activate(null);
-        final KeystoreResourceProvider keystoreResourceProvider = createKeyStoreResurceProvider(keyStoreService);
+        final KeystoreResourceProvider keystoreResourceProvider = createKeyStoreResourceProvider(keyStoreService);
         ActionRequest actionRequest =
                 Requests.newActionRequest("", KeystoreResourceProvider.ACTION_GENERATE_CERT);
         actionRequest.setContent(createGenerateCertActionContent(true));
@@ -107,21 +94,25 @@ public class KeystoreResourceProviderTest {
         //then
         final JsonValue content = result.get().getJsonContent();
         AssertJActionResponseAssert.assertThat(result).succeeded();
-        AssertJJsonValueAssert.assertThat(content).hasObject("privateKey");
+        AssertJJsonValueAssert.assertThat(content).doesNotContain("privateKey");
         checkResultForRequiredFields(content);
         checkKeyStoreEntry(content, keyStoreService);
     }
 
     @Test
-    public void testActionGenerateCertNotReturningPrivateKey() throws Exception {
+    public void testActionGenerateCSR() throws Exception {
         //given
         createKeyStores();
         final KeyStoreServiceImpl keyStoreService = new KeyStoreServiceImpl();
         keyStoreService.activate(null);
-        final KeystoreResourceProvider keystoreResourceProvider = createKeyStoreResurceProvider(keyStoreService);
+        final KeystoreResourceProvider keystoreResourceProvider = createKeyStoreResourceProvider(keyStoreService);
         ActionRequest actionRequest =
-                Requests.newActionRequest("", KeystoreResourceProvider.ACTION_GENERATE_CERT);
-        actionRequest.setContent(createGenerateCertActionContent(true));
+                Requests.newActionRequest("", KeystoreResourceProvider.ACTION_GENERATE_CSR);
+        actionRequest.setContent(
+                json(object(
+                        field("alias", TEST_CERT_ALIAS),
+                        field("CN", "openidm-csr")
+                )));
 
         //when
         final Promise<ActionResponse, ResourceException> result =
@@ -130,9 +121,9 @@ public class KeystoreResourceProviderTest {
         //then
         final JsonValue content = result.get().getJsonContent();
         AssertJActionResponseAssert.assertThat(result).succeeded();
-        AssertJJsonValueAssert.assertThat(content).hasObject("privateKey");
-        checkResultForRequiredFields(content);
-        checkKeyStoreEntry(content, keyStoreService);
+        AssertJJsonValueAssert.assertThat(content).doesNotContain("privateKey");
+        AssertJJsonValueAssert.assertThat(content).hasObject("publicKey");
+        AssertJJsonValueAssert.assertThat(content).hasString("csr");
     }
 
     @Test
@@ -141,7 +132,7 @@ public class KeystoreResourceProviderTest {
         createKeyStores();
         final KeyStoreServiceImpl keyStoreService = new KeyStoreServiceImpl();
         keyStoreService.activate(null);
-        final KeystoreResourceProvider keystoreResourceProvider = createKeyStoreResurceProvider(keyStoreService);
+        final KeystoreResourceProvider keystoreResourceProvider = createKeyStoreResourceProvider(keyStoreService);
         ActionRequest actionRequest =
                 Requests.newActionRequest("", KeystoreResourceProvider.ACTION_GENERATE_CERT);
         final JsonValue content = createGenerateCertActionContent(true);
@@ -152,6 +143,7 @@ public class KeystoreResourceProviderTest {
         final Promise<ActionResponse, ResourceException> result =
                 keystoreResourceProvider.actionInstance(new RootContext(), actionRequest);
 
+        // then
         assertThat(result).failedWithException().isInstanceOf(ResourceException.class);
     }
 
@@ -161,7 +153,7 @@ public class KeystoreResourceProviderTest {
         createKeyStores();
         final KeyStoreServiceImpl keyStoreService = new KeyStoreServiceImpl();
         keyStoreService.activate(null);
-        final KeystoreResourceProvider keystoreResourceProvider = createKeyStoreResurceProvider(keyStoreService);
+        final KeystoreResourceProvider keystoreResourceProvider = createKeyStoreResourceProvider(keyStoreService);
         ActionRequest actionRequest =
                 Requests.newActionRequest("", KeystoreResourceProvider.ACTION_GENERATE_CERT);
         actionRequest.setContent(createGenerateCertActionContent(true));
@@ -175,7 +167,7 @@ public class KeystoreResourceProviderTest {
         assertThat(result).failedWithException().isInstanceOf(ResourceException.class);
     }
 
-    private KeystoreResourceProvider createKeyStoreResurceProvider(final KeyStoreService keyStoreService)
+    private KeystoreResourceProvider createKeyStoreResourceProvider(final KeyStoreService keyStoreService)
             throws Exception {
         final KeyStoreManagementService keyStoreManagementService = mock(KeyStoreManagementService.class);
         final CryptoServiceImpl cryptoService = new CryptoServiceImpl();
@@ -183,51 +175,6 @@ public class KeystoreResourceProviderTest {
         cryptoService.activate(null);
         return new KeystoreResourceProvider("keystore", keyStoreService, new TestRepositoryService(), cryptoService,
                 keyStoreManagementService);
-    }
-
-    private void createKeyStores() throws Exception {
-        createKeyStore(IdentityServer.getFileForPath(IdentityServer.getInstance().getProperty(KEYSTORE_LOCATION)));
-        createTrustStore(IdentityServer.getFileForPath(IdentityServer.getInstance().getProperty(TRUSTSTORE_LOCATION)));
-    }
-
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    private void createKeyStore(final File keystoreFile) throws Exception {
-        keystoreFile.deleteOnExit();
-        if (keystoreFile.exists()) {
-            keystoreFile.delete();
-        }
-
-        keystoreFile.getParentFile().mkdirs();
-        Assertions.assertThat(keystoreFile.createNewFile()).isTrue().as("Unable to create keystore file");
-        try (final OutputStream outputStream = new FileOutputStream(keystoreFile)) {
-            final KeyStore keyStore =
-                    KeyStore.getInstance(IdentityServer.getInstance().getProperty(KEYSTORE_TYPE));
-            keyStore.load(null, IdentityServer.getInstance().getProperty(IdentityServer.KEYSTORE_PASSWORD).toCharArray());
-            keyStore.store(
-                    outputStream,
-                    IdentityServer.getInstance().getProperty(IdentityServer.KEYSTORE_PASSWORD).toCharArray()
-            );
-        }
-    }
-
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    private void createTrustStore(final File keystoreFile) throws Exception {
-        keystoreFile.deleteOnExit();
-        if (keystoreFile.exists()) {
-            keystoreFile.delete();
-        }
-
-        keystoreFile.getParentFile().mkdirs();
-        Assertions.assertThat(keystoreFile.createNewFile()).isTrue().as("Unable to create keystore file");
-        try (final OutputStream outputStream = new FileOutputStream(keystoreFile)) {
-            final KeyStore keyStore =
-                    KeyStore.getInstance(IdentityServer.getInstance().getProperty(TRUSTSTORE_TYPE));
-            keyStore.load(null, IdentityServer.getInstance().getProperty(TRUSTSTORE_PASSWORD).toCharArray());
-            keyStore.store(
-                    outputStream,
-                    IdentityServer.getInstance().getProperty(TRUSTSTORE_PASSWORD).toCharArray()
-            );
-        }
     }
 
     private JsonValue createGenerateCertActionContent(final boolean returnPrivateKey) {
@@ -276,78 +223,9 @@ public class KeystoreResourceProviderTest {
     }
 
     private String convertCertToPEM(final byte[] encodedCert) {
-        final StringBuilder certAsPEM = new StringBuilder();
-        certAsPEM.append(BEGIN_CERT)
-                .append(Base64.encode(encodedCert))
-                .append(END_CERT);
-        return  certAsPEM.toString();
-    }
-
-    private final class TestRepositoryService implements RepositoryService {
-
-        private final MemoryBackend repo = new MemoryBackend();
-
-        @Override
-        public ResourceResponse create(CreateRequest request) throws ResourceException {
-            try {
-                final Promise<ResourceResponse, ResourceException> promise =
-                        repo.createInstance(new RootContext(), request);
-                return promise.getOrThrow();
-            } catch (InterruptedException e) {
-                throw new InternalServerErrorException("Unable to create object in repo", e);
-            }
-        }
-
-        @Override
-        public ResourceResponse read(ReadRequest request) throws ResourceException {
-            try {
-                final Promise<ResourceResponse, ResourceException> promise =
-                        repo.readInstance(new RootContext(), request.getResourcePath(), request);
-                return promise.getOrThrow();
-            } catch (InterruptedException e) {
-                throw new InternalServerErrorException("Unable to read object in repo", e);
-            }
-        }
-
-        @Override
-        public ResourceResponse update(UpdateRequest request) throws ResourceException {
-            try {
-                final Promise<ResourceResponse, ResourceException> promise =
-                        repo.updateInstance(new RootContext(), request.getResourcePath(), request);
-                return promise.getOrThrow();
-            } catch (InterruptedException e) {
-                throw new InternalServerErrorException("Unable to update object in repo", e);
-            }
-        }
-
-        @Override
-        public ResourceResponse delete(DeleteRequest request) throws ResourceException {
-            try {
-                final Promise<ResourceResponse, ResourceException> promise =
-                        repo.deleteInstance(new RootContext(), request.getResourcePath(), request);
-                return promise.getOrThrow();
-            } catch (InterruptedException e) {
-                throw new InternalServerErrorException("Unable to delete object in repo", e);
-            }
-        }
-
-        @Override
-        public List<ResourceResponse> query(QueryRequest request) throws ResourceException {
-            try {
-                final List<ResourceResponse> resources = new LinkedList<>();
-                final Promise<QueryResponse, ResourceException> promise =
-                        repo.queryCollection(new RootContext(), request, new QueryResourceHandler() {
-                            @Override
-                            public boolean handleResource(ResourceResponse resource) {
-                                resources.add(resource);
-                                return true;
-                            }
-                        });
-                promise.getOrThrow();
-                return resources;
-            } catch (InterruptedException e) {
-                throw new InternalServerErrorException("Unable to query objects in repo", e);
-            }
-        }
+        return  ""
+                .concat(BEGIN_CERT)
+                .concat(Base64.encode(encodedCert))
+                .concat(END_CERT);
     }
 }
