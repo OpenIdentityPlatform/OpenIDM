b41d79b912206197b23dd3e43dccbcbfdddc5e25
jason.browne <jason.browne@forgerock.com>
2016-11-21 12:59:52 -0800

    OPENIDM-7084 - Add dashboard qunit

--- openidm-ui/openidm-ui-admin/src/test/qunit/org/forgerock/openidm/ui/admin/dashboard/DashboardWidgetLoaderTest.js
+++ openidm-ui/openidm-ui-admin/src/test/qunit/org/forgerock/openidm/ui/admin/dashboard/DashboardWidgetLoaderTest.js
@@ -2,4 +2,10 @@ define([
     "org/forgerock/openidm/ui/admin/dashboard/DashboardWidgetLoader"
 ], function (DashboardWidgetLoader) {
     QUnit.module('DashboardWidgetLoader Tests');
+
+    QUnit.test("Get supported widget list", function () {
+        var widgetList = DashboardWidgetLoader.getWidgetList();
+
+        QUnit.equal(widgetList["lifeCycleMemoryHeap"].defaultSize, "small", "Properly retrieved the list of supported UI widgets");
+    });
 });
