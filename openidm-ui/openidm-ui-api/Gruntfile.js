e6230698c3e46de6d67dfb3c124749771e577174
Travis Haagen <travis.haagen@forgerock.com>
2016-12-22 11:04:10 -0800

    RAPID-208 Grunt patch to fix Swagger UI issue https://github.com/swagger-api/swagger-ui/issues/2372

--- openidm-ui/openidm-ui-api/Gruntfile.js
+++ openidm-ui/openidm-ui-api/Gruntfile.js
@@ -22,7 +22,26 @@ module.exports = function (grunt) {
                     cwd: 'node_modules/swagger-ui/dist/',
                     src: ['swagger-ui.js', 'swagger-ui.min.js','css/*', 'fonts/*', 'images/*', 'lang/*', 'lib/*'],
                     dest: 'target/www/'
-                }]
+                }],
+                options: {
+                    noProcess: ['**/*.{png,gif,jpg,ico,svg,ttf,eot,woff}'],
+                    process: function (content, srcpath) {
+                        // patch for the following issue in Swagger UI, until a permanent fix is released
+                        // https://bugster.forgerock.org/jira/browse/RAPID-208
+                        // https://github.com/swagger-api/swagger-ui/issues/2372
+                        if (srcpath.indexOf('swagger-ui.js') !== -1) {
+                            return content.replace(
+                                /resource\.id = resource\.id\.replace\(\/\\s\/g, '_'\);/,
+                                "resource.id = resource.id.replace(/[^a-zA-Z0-9]/g, function(str) { return str.charCodeAt(0); });");
+                        }
+                        if (srcpath.indexOf('swagger-ui.min.js') !== -1) {
+                            return content.replace(
+                                /e\.id=e\.id.replace\(\/\\s\/g,"_"\)/,
+                                "e.id=e.id.replace(/[^a-zA-Z0-9]/g, function(str) { return str.charCodeAt(0); })");
+                        }
+                        return content;
+                    }
+                }
             },
             swaggerThemes: {
                 files: [{
