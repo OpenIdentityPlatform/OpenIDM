59be2a897af8c25b3ee938bfcd6abe490ba8e193
oliver.bradley <oliver.bradley@forgerock.com>
2017-02-22 10:16:57 -0800

    OPENIDM-7485 help text file status displayed in Update UI

--- openidm-ui/openidm-ui-admin/src/main/js/org/forgerock/openidm/ui/admin/settings/update/InstallationPreviewView.js
+++ openidm-ui/openidm-ui-admin/src/main/js/org/forgerock/openidm/ui/admin/settings/update/InstallationPreviewView.js
@@ -11,7 +11,7 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions copyright [year] [name of copyright owner]".
  *
- * Copyright 2015-2016 ForgeRock AS.
+ * Copyright 2015-2017 ForgeRock AS.
  */
 
 define([
@@ -106,6 +106,7 @@ define([
 
                 this.parentRender(() => {
                     SpinnerManager.hideSpinner();
+                    $('[data-toggle="popover"]').popover();
 
                     if (this.data.repoUpdatesExist) {
                         this.$el.find('#previewWrapper').hide();
