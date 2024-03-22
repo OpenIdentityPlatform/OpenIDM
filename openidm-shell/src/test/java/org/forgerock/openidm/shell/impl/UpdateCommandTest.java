fbc32851405ebfb99cd7bc3026120ae1e175cd35
Maxim Thomas <maxim.thomas@gmail.com>
2024-03-22 17:41:47 +0300

    Update UpdateCommandTest.java

--- openidm-shell/src/test/java/org/forgerock/openidm/shell/impl/UpdateCommandTest.java
+++ openidm-shell/src/test/java/org/forgerock/openidm/shell/impl/UpdateCommandTest.java
@@ -427,10 +427,10 @@ public class UpdateCommandTest {
                 .setQuietMode(false)
                 .setAcceptedLicense(true)
                 .setSkipRepoUpdatePreview(true)
-                .setMaxJobsFinishWaitTimeMs(100L)
-                .setCheckJobsRunningFrequency(20L)
-                .setMaxUpdateWaitTimeMs(400L)
-                .setCheckCompleteFrequency(20L);
+                .setMaxJobsFinishWaitTimeMs(1000L)
+                .setCheckJobsRunningFrequency(10L)
+                .setMaxUpdateWaitTimeMs(5000L)
+                .setCheckCompleteFrequency(10L);
         UpdateCommand updateCommand = new UpdateCommand(session, resource, config);
         UpdateExecutionState executionState = updateCommand.execute(new RootContext());
 
