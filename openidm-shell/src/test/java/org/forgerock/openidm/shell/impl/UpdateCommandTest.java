8225c1c932d2ced65ee9892f9a231f3db50c44e1
vharseko <vharseko@3a-systems.ru>
2024-04-24 16:13:01 +0300

    [#13 #9] FIX build + IT test (#20)

--- openidm-shell/src/test/java/org/forgerock/openidm/shell/impl/UpdateCommandTest.java
+++ openidm-shell/src/test/java/org/forgerock/openidm/shell/impl/UpdateCommandTest.java
@@ -427,9 +427,9 @@ public class UpdateCommandTest {
                 .setQuietMode(false)
                 .setAcceptedLicense(true)
                 .setSkipRepoUpdatePreview(true)
-                .setMaxJobsFinishWaitTimeMs(50L)
+                .setMaxJobsFinishWaitTimeMs(500L)
                 .setCheckJobsRunningFrequency(10L)
-                .setMaxUpdateWaitTimeMs(200L)
+                .setMaxUpdateWaitTimeMs(2000L)
                 .setCheckCompleteFrequency(10L);
         UpdateCommand updateCommand = new UpdateCommand(session, resource, config);
         UpdateExecutionState executionState = updateCommand.execute(new RootContext());
@@ -488,9 +488,9 @@ public class UpdateCommandTest {
                 .setQuietMode(false)
                 .setAcceptedLicense(true)
                 .setSkipRepoUpdatePreview(true)
-                .setMaxJobsFinishWaitTimeMs(50L)
+                .setMaxJobsFinishWaitTimeMs(500L)
                 .setCheckJobsRunningFrequency(10L)
-                .setMaxUpdateWaitTimeMs(200L)
+                .setMaxUpdateWaitTimeMs(2000L)
                 .setCheckCompleteFrequency(10L);
         UpdateCommand updateCommand = new UpdateCommand(session, resource, config);
         UpdateExecutionState executionState = updateCommand.execute(new RootContext());
