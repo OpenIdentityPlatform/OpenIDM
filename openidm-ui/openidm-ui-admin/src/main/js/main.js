e370a0d6cd4c3a79fbc03c24f3768586812376e9
maximthomas <maxim.thomas@gmail.com>
2024-04-24 19:11:26 +0300

    bump backgrid version, fix `filter.query is not a function.`

--- openidm-ui/openidm-ui-admin/src/main/js/main.js
+++ openidm-ui/openidm-ui-admin/src/main/js/main.js
@@ -32,7 +32,7 @@ require.config({
         "backbone.paginator": "libs/backbone.paginator.min-2.0.2-min",
         "backbone-relational": "libs/backbone-relational-0.9.0-min",
         "backgrid": "libs/backgrid.min-0.3.5-min",
-        "backgrid-filter": "libs/backgrid-filter.min-0.3.5-min",
+        "backgrid-filter": "libs/backgrid-filter.min-0.3.7-min",
         "backgrid-paginator": "libs/backgrid-paginator.min-0.3.5-min",
         "backgrid-selectall": "libs/backgrid-select-all-0.3.5-min",
         lodash: "libs/lodash-3.10.1-min",
